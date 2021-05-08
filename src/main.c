a250d72b09daea2132f39ecb8d85283fa53d3fd9
Frederik Engels <frederik.engels92@gmail.com>
2021-05-08 23:50:53 +0100

    test out the lexing process

--- src/main.c
+++ src/main.c
@@ -12,7 +12,16 @@ int main(int argc, char** argv)
     ElyLexer lexer;
     ely_lex_create(&lexer, src, len);
     ElyToken toks[32];
+    uint32_t read = ely_lex_src(&lexer, toks, 32);
     printf("read %d tokens\n", read);
+
+    uint32_t offset = 0;
+    for (uint32_t i = 0; i != read; ++i)
+    {
+        ElyToken tok = toks[i];
+        printf("%d %.*s\n", tok.len, tok.len, &src[offset]);
+        offset += tok.len;
+    }
 
     ElyReader reader;
     ely_reader_create(&reader, filename);
