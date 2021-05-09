ad1c598cc119d90a95f121ab479f39a05d431342
Frederik Engels <frederik.engels92@gmail.com>
2021-05-09 01:04:59 +0100

    test out various literals to lex

--- src/main.c
+++ src/main.c
@@ -6,9 +6,10 @@
 int main(int argc, char** argv)
 {
     const char* filename = "test";
-    const char* src      = "(def (f x) x)";
-    size_t      len      = strlen(src);
+    const char* src = "(def (f x) #:x)\n123.123\n123\n123.\n123fd\n13.34fds\n";
+    size_t      len = strlen(src);
 
+    printf("source:\n%s\n", src);
     ElyLexer lexer;
     ely_lex_create(&lexer, src, len);
     ElyToken toks[32];
@@ -19,7 +20,14 @@ int main(int argc, char** argv)
     for (uint32_t i = 0; i != read; ++i)
     {
         ElyToken tok = toks[i];
-        printf("%d %.*s\n", tok.len, tok.len, &src[offset]);
+        if (tok.kind == ELY_TOKEN_NEWLINE_LF)
+        {
+            printf("%d \"\\n\"\n", tok.len);
+        }
+        else
+        {
+            printf("%d \"%.*s\"\n", tok.len, tok.len, &src[offset]);
+        }
         offset += tok.len;
     }
 
