83b164cdd54de436fa6ffd08e9af6989f8491f4d
Frederik Engels <frederik.engels92@gmail.com>
2021-05-10 01:07:59 +0100

    add buffer for copying data over

--- include/ely/reader.h
+++ include/ely/reader.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #include "ely/export.h"
 #include "ely/lexer.h"
+#include "ely/list.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,9 +43,20 @@ typedef struct ElyStxLocation
     ElyPosition end_pos;
 } ElyStxLocation;
 
+typedef struct ElyNode
+{
+    ElyList        link;
+    enum ElyStx    type;
+    ElyStxLocation loc;
+    // this doesn't need an explicit alignment parameter as it should follow
+    // loc which has void* alignment.
+    char data[];
+} ElyNode;
+
+uint32_t ely_node_sizeof(const ElyNode* node);
+
 typedef struct ElyStxNode
 {
-
     void*    data;
     uint32_t len;
     // both of these get packed into the 4 bytes behind len
@@ -52,6 +65,74 @@ typedef struct ElyStxNode
 
     ElyStxLocation location;
 } ElyStxNode;
+
+typedef struct ElyNodeParensList
+{
+    ElyList list;
+} ElyNodeParensList;
+
+typedef struct ElyNodeBracketList
+{
+    ElyList list;
+} ElyNodeBracketList;
+
+typedef struct ElyNodeBraceList
+{
+    ElyList list;
+} ElyNodeBraceList;
+
+typedef struct ElyNodeIdentifier
+{
+    uint32_t len;
+    // TODO: intern the identifier name
+    char str[];
+} ElyNodeIdentifier;
+
+uint32_t ely_node_identifier_sizeof(const ElyNodeIdentifier* node);
+
+typedef struct ElyNodeKeywordLit
+{
+    uint32_t len;
+    char     str[];
+} ElyNodeKeywordLit;
+
+uint32_t ely_node_keyword_lit_sizeof(const ElyNodeKeywordLit* node);
+
+typedef struct ElyNodeStringLit
+{
+    size_t len;
+    char   str[];
+} ElyNodeStringLit;
+
+uint32_t ely_node_string_lit_sizeof(const ElyNodeStringLit* node);
+
+typedef struct ElyNodeIntLit
+{
+    uint32_t len;
+    char     str[];
+} ElyNodeIntLit;
+
+uint32_t ely_node_int_lit_sizeof(const ElyNodeIntLit* node);
+
+typedef struct ElyNodeFloatLit
+{
+    uint32_t len;
+    char     str[];
+} ElyNodeFloatLit;
+
+uint32_t ely_node_float_lit_sizeof(const ElyNodeFloatLit* node);
+
+typedef struct ElyNodeTrueLit
+{
+} ElyNodeTrueLit;
+
+uint32_t ely_node_true_lit_sizeof(const ElyNodeTrueLit*);
+
+typedef struct ElyNodeFalseLit
+{
+} ElyNodeFalseLit;
+
+uint32_t ely_node_false_lit_sizeof(const ElyNodeFalseLit*);
 
 ELY_EXPORT void ely_stx_node_create(ElyStxNode* __restrict__ stx,
                                     enum ElyStx kind,
@@ -64,11 +145,14 @@ typedef struct ElyReader
 {
     const char* filename;
 
+    char* buffer;
+
     ElyPosition current_pos;
     uint32_t    current_byte;
 } ElyReader;
 
 ELY_EXPORT void ely_reader_create(ElyReader* reader, const char* filename);
+ELY_EXPORT void ely_reader_destroy(ElyReader* reader);
 
 ELY_EXPORT ElyStxNode*
            ely_reader_read(ElyReader* reader, ElyToken* tokens, uint32_t len);
--- src/reader.c
+++ src/reader.c
@@ -4,6 +4,118 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "ely/defines.h"
+
+static inline ElyStxNode*
+read_paren_list(ElyReader* reader, ElyToken* tokens, uint32_t len)
+{
+    ElyStxNode* node_list[32];
+    (void) node_list;
+    for (uint32_t i = 0; i != len; ++i)
+    {
+        ElyToken tok = tokens[i];
+
+        switch (tok.kind)
+        {
+        case ELY_TOKEN_LPAREN:
+            break;
+        case ELY_TOKEN_RPAREN:
+            break;
+        }
+    }
+}
+
+static inline ElyStxNode*
+read_bracket_list(ElyReader* reader, ElyToken* tokens, uint32_t len)
+{
+    (void) reader;
+    (void) tokens;
+    (void) len;
+}
+
+static inline ElyStxNode*
+read_brace_list(ElyReader* reader, ElyToken* tokens, uint32_t len)
+{
+    (void) reader;
+    (void) tokens;
+    (void) len;
+}
+
+uint32_t ely_node_sizeof(const ElyNode* node)
+{
+    uint32_t data_size;
+    switch (node->type)
+    {
+    case ELY_STX_PARENS_LIST:
+        data_size = sizeof(ElyNodeParensList);
+    case ELY_STX_BRACKET_LIST:
+        data_size = sizeof(ElyNodeBracketList);
+    case ELY_STX_BRACE_LIST:
+        data_size = sizeof(ElyNodeBraceList);
+    case ELY_STX_IDENTIFIER:
+        data_size =
+            ely_node_identifier_sizeof((const ElyNodeIdentifier*) node->data);
+    case ELY_STX_KEYWORD_LIT:
+        data_size =
+            ely_node_keyword_lit_sizeof((const ElyNodeKeywordLit*) node->data);
+    case ELY_STX_STRING_LIT:
+        data_size =
+            ely_node_string_lit_sizeof((const ElyNodeStringLit*) node->data);
+    case ELY_STX_INT_LIT:
+        data_size = ely_node_int_lit_sizeof((const ElyNodeIntLit*) node->data);
+    case ELY_STX_FLOAT_LIT:
+        data_size =
+            ely_node_float_lit_sizeof((const ElyNodeFloatLit*) node->data);
+    case ELY_STX_TRUE_LIT:
+        data_size =
+            ely_node_true_lit_sizeof((const ElyNodeTrueLit*) node->data);
+    case ELY_STX_FALSE_LIT:
+        data_size =
+            ely_node_false_lit_sizeof((const ElyNodeFalseLit*) node->data);
+    default:
+        __builtin_unreachable();
+    }
+
+    return data_size + sizeof(ElyNode);
+}
+
+uint32_t ely_node_identifier_sizeof(const ElyNodeIdentifier* node)
+{
+    return sizeof(ElyNodeIdentifier) + node->len;
+}
+
+uint32_t ely_node_keyword_lit_sizeof(const ElyNodeKeywordLit* node)
+{
+    return sizeof(ElyNodeKeywordLit) + node->len;
+}
+
+uint32_t ely_node_string_lit_sizeof(const ElyNodeStringLit* node)
+{
+    return sizeof(ElyNodeStringLit) + node->len;
+}
+
+uint32_t ely_node_int_lit_sizeof(const ElyNodeIntLit* node)
+{
+    return sizeof(ElyNodeIntLit) + node->len;
+}
+
+uint32_t ely_node_float_lit_sizeof(const ElyNodeFloatLit* node)
+{
+    return sizeof(ElyNodeFloatLit) + node->len;
+}
+
+uint32_t ely_node_true_lit_sizeof(const ElyNodeTrueLit* node)
+{
+    (void) node;
+    return sizeof(ElyNodeTrueLit);
+}
+
+uint32_t ely_node_false_lit_sizeof(const ElyNodeFalseLit* node)
+{
+    (void) node;
+    return sizeof(ElyNodeFalseLit);
+}
+
 void ely_stx_node_create(ElyStxNode* __restrict__ stx,
                          enum ElyStx kind,
                          void* __restrict__ data,
@@ -47,8 +159,10 @@ void ely_stx_node_destroy(ElyStxNode* stx)
     case ELY_STX_INT_LIT:
     case ELY_STX_FLOAT_LIT:
         free(stx->data);
+        break;
     case ELY_STX_TRUE_LIT:
     case ELY_STX_FALSE_LIT:
+        break;
     default:
         __builtin_unreachable();
     }
@@ -57,9 +171,15 @@ void ely_stx_node_destroy(ElyStxNode* stx)
 void ely_reader_create(ElyReader* reader, const char* filename)
 {
     reader->filename        = filename;
+    reader->buffer          = NULL;
     reader->current_pos.row = 1;
     reader->current_pos.col = 1;
     reader->current_byte    = 0;
+}
+
+void ely_reader_destroy(ElyReader* reader)
+{
+    free(reader->buffer);
 }
 
 ElyStxNode* ely_reader_read(ElyReader* reader, ElyToken* tokens, uint32_t len)
@@ -78,20 +198,43 @@ ElyStxNode* ely_reader_read(ElyReader* reader, ElyToken* tokens, uint32_t len)
             ++reader->current_pos.row;
             break;
         case ELY_TOKEN_COMMENT:
-
+            break;
         case ELY_TOKEN_LPAREN:
-        case ELY_TOKEN_RPAREN:
+            ELY_MUSTTAIL return read_paren_list(reader, &tokens[i], len - i);
         case ELY_TOKEN_LBRACKET:
-        case ELY_TOKEN_RBRACKET:
+            ELY_MUSTTAIL return read_bracket_list(reader, &tokens[i], len - i);
         case ELY_TOKEN_LBRACE:
+            ELY_MUSTTAIL return read_brace_list(reader, &tokens[i], len - i);
+        case ELY_TOKEN_RPAREN:
+        case ELY_TOKEN_RBRACKET:
+            assert(false && "Unexpected closing");
         case ELY_TOKEN_RBRACE:
 
-        case ELY_TOKEN_ID:
-
-        case ELY_TOKEN_INT_LIT:
-        case ELY_TOKEN_FLOAT_LIT:
-        case ELY_TOKEN_STRING_LIT:
-        case ELY_TOKEN_KEYWORD_LIT:
+        case ELY_TOKEN_ID: {
+            ElyStxNode* node = malloc(sizeof(ElyStxNode));
+            ely_stx_node_create(node, ELY_STX_IDENTIFIER, NULL, 0);
+            return node;
+        }
+        case ELY_TOKEN_INT_LIT: {
+            ElyStxNode* node = malloc(sizeof(ElyStxNode));
+            ely_stx_node_create(node, ELY_STX_INT_LIT, NULL, 0);
+            return node;
+        }
+        case ELY_TOKEN_FLOAT_LIT: {
+            ElyStxNode* node = malloc(sizeof(ElyStxNode));
+            ely_stx_node_create(node, ELY_STX_FLOAT_LIT, NULL, 0);
+            return node;
+        }
+        case ELY_TOKEN_STRING_LIT: {
+            ElyStxNode* node = malloc(sizeof(ElyStxNode));
+            ely_stx_node_create(node, ELY_STX_STRING_LIT, NULL, 0);
+            return node;
+        }
+        case ELY_TOKEN_KEYWORD_LIT: {
+            ElyStxNode* node = malloc(sizeof(ElyStxNode));
+            ely_stx_node_create(node, ELY_STX_KEYWORD_LIT, NULL, 0);
+            return node;
+        }
         case ELY_TOKEN_TRUE_LIT: {
             ElyStxNode* node = malloc(sizeof(ElyStxNode));
             ely_stx_node_create(node, ELY_STX_TRUE_LIT, NULL, 0);
@@ -108,6 +251,7 @@ ElyStxNode* ely_reader_read(ElyReader* reader, ElyToken* tokens, uint32_t len)
             __builtin_unreachable();
         }
 
+        // TODO: increment columns and rows
         reader->current_byte += tok.len;
     }
 }
