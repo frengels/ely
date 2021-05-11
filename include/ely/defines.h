09c16f43d7e2d5b765ce104cb01054ce4c3784ba
Frederik Engels <frederik.engels92@gmail.com>
2021-05-11 22:29:57 +0100

    add macro for always_inline

--- include/ely/defines.h
+++ include/ely/defines.h
@@ -9,3 +9,13 @@
 #if !defined(ELY_MUSTTAIL)
 #define ELY_MUSTTAIL
 #endif
+
+#if defined(__has_attribute)
+#if __has_attribute(always_inline)
+#define ELY_ALWAYS_INLINE __attribute__((always_inline))
+#endif
+#endif
+
+#if !defined(ELY_ALWAYS_INLINE)
+#define ELY_ALWAYS_INLINE inline
+#endif
