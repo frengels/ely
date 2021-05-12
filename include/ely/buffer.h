a39af8c684861e92044844fe802894a5ac8b45aa
Frederik Engels <frederik.engels92@gmail.com>
2021-05-12 23:50:31 +0100

    add handy functions for using the buffer

--- include/ely/buffer.h
+++ include/ely/buffer.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
+
+#include "ely/defines.h"
 
 /// size and capacity should be according to the callee's specified format
 /// data is expected to be cast to the target type before usage
@@ -14,3 +17,15 @@ typedef struct ElyBuffer
     // the total capacity of data
     uint32_t capacity;
 } ElyBuffer;
+
+#define ely_buffer_peek(ty, buffer) ((ty*) buffer->data)[buffer->begin]
+
+static ELY_ALWAYS_INLINE void ely_buffer_advance(ElyBuffer* buffer)
+{
+    ++buffer->begin;
+}
+
+static ELY_ALWAYS_INLINE bool ely_buffer_empty(const ElyBuffer* buffer)
+{
+    return buffer->begin == buffer->end;
+}
