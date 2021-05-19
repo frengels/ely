c8b2cdeaac62a0bbd5ae8a2530dbceaa39e301f7
Frederik Engels <frederik.engels92@gmail.com>
2021-05-20 00:29:40 +0100

    add a benchmark for reading

--- benchmark/lex.cpp
+++ benchmark/lex.cpp
@@ -1,9 +1,11 @@
 #include <benchmark/benchmark.h>
 
 #include <array>
+#include <memory>
 #include <string>
 
 #include <ely/lexer.h>
+#include <ely/reader.h>
 #include <ely/token.h>
 
 constexpr int lines = 10000;
@@ -36,7 +38,7 @@ static void BM_lex(benchmark::State& state)
     for (auto _ : state)
     {
         auto        src_len   = src.size();
-        const char* src_p       = src.data();
+        const char* src_p     = src.data();
         ElyToken*   tok_buf_p = tok_buf.data();
         auto        buf_len   = tok_buf.size();
         while (true)
@@ -61,5 +63,41 @@ static void BM_lex(benchmark::State& state)
     state.SetItemsProcessed(lines * state.iterations());
 }
 BENCHMARK(BM_lex);
+
+static void BM_read(benchmark::State& state)
+{
+    auto src = long_source();
+    // make sure we have enough space
+    static constexpr std::size_t token_buf_len = 1000000;
+    auto token_buf = std::make_unique<ElyToken[]>(token_buf_len);
+
+    auto        src_len = src.size();
+    const char* src_p   = src.data();
+
+    auto lex_res = ely_lex(src_p, src_len, token_buf.get(), token_buf_len);
+    assert(lex_res.bytes_processed == src_len);
+
+    for (auto _ : state)
+    {
+        ElyReader reader;
+        ely_reader_create(&reader, "hello_world.ely");
+
+        ElyList nodes;
+        ely_list_create(&nodes);
+
+        ely_reader_read_all(
+            &reader, src.data(), token_buf.get(), lex_res.tokens_read, &nodes);
+
+        ElyNode *e, *tmp;
+        ely_list_for_each_safe(e, tmp, &nodes, link)
+        {
+            ely_node_destroy(e);
+        }
+    }
+
+    state.SetBytesProcessed(src_len * state.iterations());
+    state.SetItemsProcessed(lines * state.iterations());
+}
+BENCHMARK(BM_read);
 
 BENCHMARK_MAIN();
