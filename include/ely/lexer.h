7431737de9deb22e028db220bd48d41d9da433fd
Frederik Engels <frederik.engels92@gmail.com>
2021-05-23 21:01:32 +0100

    add a lexing method using the c++ scanner

--- include/ely/lexer.h
+++ include/ely/lexer.h
@@ -7,6 +7,11 @@
 #include "ely/export.h"
 
 #ifdef __cplusplus
+#include "ely/scanner.hpp"
+#include <string_view>
+#endif
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -18,11 +23,37 @@ typedef struct ElyLexResult
     uint32_t tokens_read;
 } ElyLexResult;
 
-ELY_EXPORT ELY_NODISCARD ElyLexResult
-ely_lex(const char* __restrict__ src,
-        size_t src_len,
-        ElyToken* __restrict__ token_buf,
-        size_t buf_len);
+ELY_EXPORT ELY_NODISCARD ElyLexResult ely_lex(const char* __restrict__ src,
+                                              size_t src_len,
+                                              ElyToken* __restrict__ token_buf,
+                                              size_t buf_len);
+
+#ifdef __cplusplus
+ELY_NODISCARD ELY_NOINLINE ElyLexResult
+ely_lex_scanner(std::string_view                         src,
+                ely::Lexeme<std::string_view::iterator>* lexeme_buf,
+                size_t                                   buf_len)
+{
+    auto scanner = ely::Scanner{src.begin(), src.end()};
+
+    auto   scanner_it = scanner.begin();
+    size_t buf_i      = 0;
+    for (; buf_i != buf_len; ++buf_i)
+    {
+        if (scanner_it == scanner.end())
+        {
+            break;
+        }
+
+        lexeme_buf[buf_i] = *scanner_it++;
+    }
+
+    ElyLexResult res{static_cast<uint32_t>(scanner_it.base() - src.begin()),
+                     static_cast<uint32_t>(buf_i)};
+
+    return res;
+}
+#endif
 
 #ifdef __cplusplus
 }
