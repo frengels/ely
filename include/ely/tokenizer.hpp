0758e63c3c8178bccb952830c33c105812f15e9a
Frederik Engels <frederik.engels92@gmail.com>
2021-05-23 23:57:43 +0100

    add a type for Atmosphere as well
    additionally make all the types safe to destruct and construct properly

--- include/ely/tokenizer.hpp
+++ include/ely/tokenizer.hpp
@@ -32,31 +32,165 @@ enum class TokenKind : uint8_t
 #undef CAST
 };
 
+enum class AtmosphereKind : uint8_t
+{
+#define CAST(x) static_cast<std::underlying_type_t<LexemeKind>>(x)
+    Whitespace  = CAST(LexemeKind::Whitespace),
+    Tab         = CAST(LexemeKind::Tab),
+    NewlineCr   = CAST(LexemeKind::NewlineCr),
+    NewlineLf   = CAST(LexemeKind::NewlineLf),
+    NewlineCrlf = CAST(LexemeKind::NewlineCrlf),
+    Comment     = CAST(LexemeKind::Comment),
+#undef CAST
+};
+
+namespace atmosphere
+{
+class Whitespace
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::Whitespace;
+
+private:
+    std::size_t len;
+
+public:
+    constexpr Whitespace(std::size_t len) : len(len)
+    {}
+
+    constexpr std::size_t size() const
+    {
+        return len;
+    }
+};
+
+class Tab
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::Tab;
+
+private:
+    std::size_t len;
+
+public:
+    constexpr Tab(std::size_t len) : len(len)
+    {}
+
+    constexpr std::size_t size() const
+    {
+        return len;
+    }
+};
+
+class NewlineCr
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::NewlineCr;
+
+public:
+    constexpr std::size_t size() const
+    {
+        return 1;
+    }
+};
+
+class NewlineLf
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::NewlineLf;
+
+public:
+    constexpr std::size_t size() const
+    {
+        return 1;
+    }
+};
+
+class NewlineCrlf
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::NewlineCrlf;
+
+public:
+    constexpr std::size_t size() const
+    {
+        return 2;
+    }
+};
+
+class Comment
+{
+public:
+    static constexpr AtmosphereKind enum_value = AtmosphereKind::Comment;
+
+private:
+    std::string str;
+
+public:
+    Comment() = default;
+
+    template<typename... Args>
+    constexpr Comment(std::in_place_t, Args&&... args)
+        : str(static_cast<Args&&>(args)...)
+    {}
+
+    constexpr std::size_t size() const
+    {
+        return 1 + str.size();
+    }
+};
+} // namespace atmosphere
+
 namespace token
 {
 class Eof
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::Eof;
+};
 
 class LParen
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::LParen;
+};
 
 class RParen
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::RParen;
+};
 
 class LBracket
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::LBracket;
+};
 
 class RBracket
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::RBracket;
+};
 
 class LBrace
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::LBrace;
+};
 
 class RBrace
-{};
+{
+public:
+    static constexpr TokenKind enum_value = TokenKind::RBrace;
+};
 
 class Identifier
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::Identifier;
+
+private:
     std::string name;
 
 public:
@@ -70,6 +204,10 @@ public:
 
 class IntLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::IntLit;
+
+private:
     std::string str;
 
 public:
@@ -83,6 +221,10 @@ public:
 
 class FloatLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::FloatLit;
+
+private:
     std::string str;
 
 public:
@@ -96,6 +238,10 @@ public:
 
 class CharLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::CharLit;
+
+private:
     std::string str;
 
 public:
@@ -109,6 +255,10 @@ public:
 
 class StringLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::StringLit;
+
+private:
     std::string str;
 
 public:
@@ -122,6 +272,10 @@ public:
 
 class KeywordLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::KeywordLit;
+
+private:
     std::string str;
 
 public:
@@ -135,6 +289,10 @@ public:
 
 class BoolLit
 {
+public:
+    static constexpr TokenKind enum_value = TokenKind::BoolLit;
+
+private:
     bool b;
 
 public:
@@ -152,10 +310,58 @@ public:
 
 namespace detail
 {
+union AtmosphereUnion
+{
+#define DEFINE_CONSTRUCTOR(variant, member)                                    \
+    template<typename... Args>                                                 \
+    explicit constexpr AtmosphereUnion(std::in_place_type_t<variant>,          \
+                                       Args&&... args)                         \
+        : member(static_cast<Args&&>(args)...)                                 \
+    {}
+
+    DEFINE_CONSTRUCTOR(atmosphere::Whitespace, whitespace)
+    DEFINE_CONSTRUCTOR(atmosphere::Tab, tab)
+    DEFINE_CONSTRUCTOR(atmosphere::NewlineCr, newline_cr)
+    DEFINE_CONSTRUCTOR(atmosphere::NewlineLf, newline_lf)
+    DEFINE_CONSTRUCTOR(atmosphere::NewlineCrlf, newline_crlf)
+    DEFINE_CONSTRUCTOR(atmosphere::Comment, comment)
+#undef DEFINE_CONSTRUCTOR
+
+    ~AtmosphereUnion()
+    {}
+
+    atmosphere::Whitespace  whitespace;
+    atmosphere::Tab         tab;
+    atmosphere::NewlineCr   newline_cr;
+    atmosphere::NewlineLf   newline_lf;
+    atmosphere::NewlineCrlf newline_crlf;
+    atmosphere::Comment     comment;
+};
+
 union TokenUnion
 {
-    TokenUnion()
+#define DEFINE_CONSTRUCTOR(variant, member)                                    \
+    template<typename... Args>                                                 \
+    explicit constexpr TokenUnion(std::in_place_type_t<variant>,               \
+                                  Args&&... args)                              \
+        : member(static_cast<Args&&>(args)...)                                 \
     {}
+
+    DEFINE_CONSTRUCTOR(token::Eof, eof)
+    DEFINE_CONSTRUCTOR(token::LParen, lparen)
+    DEFINE_CONSTRUCTOR(token::RParen, rparen)
+    DEFINE_CONSTRUCTOR(token::LBracket, lbracket)
+    DEFINE_CONSTRUCTOR(token::RBracket, rbracket)
+    DEFINE_CONSTRUCTOR(token::LBrace, lbrace)
+    DEFINE_CONSTRUCTOR(token::RBrace, rbrace)
+    DEFINE_CONSTRUCTOR(token::Identifier, identifier)
+    DEFINE_CONSTRUCTOR(token::IntLit, int_lit)
+    DEFINE_CONSTRUCTOR(token::FloatLit, float_lit)
+    DEFINE_CONSTRUCTOR(token::CharLit, char_lit)
+    DEFINE_CONSTRUCTOR(token::StringLit, string_lit)
+    DEFINE_CONSTRUCTOR(token::KeywordLit, keyword_lit)
+    DEFINE_CONSTRUCTOR(token::BoolLit, bool_lit)
+#undef DEFINE_CONSTRUCTOR
 
     ~TokenUnion()
     {}
@@ -179,14 +385,100 @@ union TokenUnion
 };
 } // namespace detail
 
+class ELY_EXPORT Atmosphere
+{
+    AtmosphereKind          kind;
+    detail::AtmosphereUnion values;
+
+public:
+    template<typename T, typename... Args>
+    explicit constexpr Atmosphere(std::in_place_type_t<T>, Args&&... args)
+        : kind(T::enum_value),
+          values(std::in_place_type<T>, static_cast<Args&&>(args)...)
+    {}
+
+    template<typename F>
+    constexpr auto visit(F&& fn) const& -> decltype(auto)
+    {
+#define CALL(member) static_cast<F&&>(fn)(member)
+        switch (kind)
+        {
+        case AtmosphereKind::Whitespace:
+            return CALL(values.whitespace);
+        case AtmosphereKind::Tab:
+            return CALL(values.tab);
+        case AtmosphereKind::NewlineCr:
+            return CALL(values.newline_cr);
+        case AtmosphereKind::NewlineLf:
+            return CALL(values.newline_lf);
+        case AtmosphereKind::NewlineCrlf:
+            return CALL(values.newline_crlf);
+        case AtmosphereKind::Comment:
+            return CALL(values.comment);
+        default:
+            __builtin_unreachable();
+        }
+#undef CALL
+    }
+
+    template<typename F>
+    constexpr auto visit(F&& fn) & -> decltype(auto)
+    {
+        return static_cast<const Atmosphere&>(*this).visit(
+            [&](const auto& x) -> decltype(auto) {
+                using ty =
+                    std::remove_cv_t<std::remove_reference_t<decltype(x)>>;
+                return static_cast<F&&>(fn)(const_cast<ty&>(x));
+            });
+    }
+
+    template<typename F>
+    constexpr auto visit(F&& fn) && -> decltype(auto)
+    {
+        return visit([&](auto& x) -> decltype(auto) {
+            return static_cast<F&&>(fn)(std::move(x));
+        });
+    }
+
+    template<typename F>
+    constexpr auto visit(F&& fn) const&& -> decltype(auto)
+    {
+        return static_cast<const Atmosphere&>(*this).visit(
+            [&](const auto& x) -> decltype(auto) {
+                return static_cast<F&&>(fn)(std::move(x));
+            });
+    }
+
+    ~Atmosphere()
+    {
+        visit([](auto& x) {
+            using ty = std::remove_cv_t<std::remove_reference_t<decltype(x)>>;
+            if constexpr (!std::is_trivially_destructible_v<ty>)
+            {
+                x.~ty();
+            }
+        });
+    }
+};
+
 /// unlike a Lexeme, a Token owns the data it holds. Additionally it holds
 /// preceding atmosphere and any trailing atmosphere until the next newline.
 class ELY_EXPORT Token
 {
+private:
     TokenKind          kind;
     detail::TokenUnion values;
 
-    Token() = default;
+public:
+    constexpr Token()
+        : kind(TokenKind::Eof), values(std::in_place_type<token::Eof>)
+    {}
+
+    template<typename T, typename... Args>
+    explicit constexpr Token(std::in_place_type_t<T>, Args&&... args)
+        : kind(T::enum_value),
+          values(std::in_place_type<T>, static_cast<Args&&>(args)...)
+    {}
 
     template<typename F>
     constexpr auto visit(F&& fn) const& -> decltype(auto)
@@ -251,7 +543,9 @@ class ELY_EXPORT Token
     constexpr auto visit(F&& fn) const&& -> decltype(auto)
     {
         return static_cast<const Token&>(*this).visit(
-            [&](const auto& x) { return static_cast<F&&>(fn)(std::move(x)); });
+            [&](const auto& x) -> decltype(auto) {
+                return static_cast<F&&>(fn)(std::move(x));
+            });
     }
 
     ~Token()
