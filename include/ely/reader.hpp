ffc7c8611d0f86cf1e291fbb777d3d07ddef82b8
Frederik Engels <frederik.engels92@gmail.com>
2021-06-10 00:19:13 +0100

    start implementing reading through overloads

--- include/ely/reader.hpp
+++ include/ely/reader.hpp
@@ -24,6 +24,57 @@ public:
     constexpr std::optional<ast::Syntax> next()
     {
         return std::nullopt;
+
+        auto token = tok_stream_.next();
+
+        return next_impl(std::move(token));
+    }
+
+private:
+    constexpr std::optional<ast::Syntax> next_impl(ely::Token&& token)
+    {
+
+        return std::move(token).visit(
+            [&](auto&& tok) -> std::optional<ast::Syntax> {
+                using tok_ty = std::remove_cvref_t<decltype(tok)>;
+
+                if constexpr (std::is_same_v<ely::token::LParen, tok_ty>)
+                {
+                    return read_list(std::move(tok), std::move(token));
+                }
+                else if constexpr (std::is_same_v<ely::token::Eof, tok_ty>)
+                {
+                    return std::nullopt;
+                }
+            });
+    }
+
+    constexpr ast::Syntax read(ely::token::StringLit str_lit, ely::Token tok)
+    {
+
+    }
+
+    constexpr ast::Syntax read(ely::token::LParen, ely::Token tok)
+    {
+        std::vector<ely::ast::Syntax> values{};
+        auto                          tok_next = tok_stream_.next();
+
+        std::move(tok_next).visit([&](auto&& tok) {
+            using tok_ty = std::remove_cvref_t<decltype(tok)>;
+
+            if constexpr (std::is_same_v<ely::token::RParen, tok_ty>)
+            {
+                auto list = ely::ast::List(std::move(values));
+                auto stx  = ely::ast::Syntax(std::move(list),
+                                            ely::ast::LexicalContext{});
+                return std::optional<ast::Syntax>(std::move(stx));
+            }
+            else
+            {
+                auto stx = read(std::move(tok), std::move(token));
+                values.emplace_back(std::move(stx));
+            }
+        });
     }
 };
 } // namespace ely
