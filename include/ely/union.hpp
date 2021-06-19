72741e0af54dafebb2411d2f434286a236d310f6
Frederik Engels <frederik.engels92@gmail.com>
2021-06-19 12:59:26 +0100

    Union can be used generically everywhere instead of union

---
+++ include/ely/union.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <type_traits>
+
+namespace ely
+{
+template<typename... Args>
+class Union;
+
+template<>
+class Union<>
+{};
+
+template<typename T, typename... Rest>
+class Union<T, Rest...>
+{
+private:
+    using first_type = T;
+    using rest_type  = Union<Rest...>;
+
+    static constexpr auto rest_size = sizeof...(Rest);
+
+    static constexpr bool destructible =
+        std::is_destructible_v<T> && (std::is_destructible_v<Rest> && ...);
+    static constexpr bool trivially_destructible =
+        std::is_trivially_destructible_v<T> &&
+        (std::is_trivially_destructible_v<Rest> && ...);
+
+    static constexpr bool copy_construct =
+        std::is_copy_constructible_v<T> &&
+        (std::is_copy_constructible_v<Rest> && ...);
+    static constexpr bool trivially_copy_construct =
+        std::is_trivially_copy_constructible_v<T> &&
+        (std::is_trivially_copy_constructible_v<Rest> && ...);
+
+    static constexpr bool move_construct =
+        std::is_move_constructible_v<T> &&
+        (std::is_move_constructible_v<Rest> && ...);
+    static constexpr bool trivially_move_construct =
+        std::is_trivially_move_constructible_v<T> &&
+        (std::is_trivially_move_constructible_v<Rest> && ...);
+
+    static constexpr bool copy_assign =
+        std::is_copy_assignable_v<T> &&
+        (std::is_copy_assignable_v<Rest> && ...);
+    static constexpr bool trivially_copy_assign =
+        std::is_trivially_copy_assignable_v<T> &&
+        (std::is_trivially_copy_assignable_v<Rest> && ...);
+
+    static constexpr bool move_assign =
+        std::is_move_assignable_v<T> &&
+        (std::is_move_assignable_v<Rest> && ...);
+    static constexpr bool trivially_move_assign =
+        std::is_trivially_move_assignable_v<T> &&
+        (std::is_trivially_move_assignable_v<Rest> && ...);
+
+private:
+    union
+    {
+        first_type first;
+        rest_type  rest;
+    };
+
+public:
+    Union() = default;
+
+    template<std::size_t I, typename... Args>
+    explicit(sizeof...(Args) == 0) constexpr Union(std::in_place_index_t<I>,
+                                                   Args&&... args) requires(I ==
+                                                                            0)
+        : first(static_cast<Args&&>(args)...)
+    {}
+
+    template<std::size_t I, typename... Args>
+    explicit(sizeof...(Args) == 0) constexpr Union(std::in_place_index_t<I>,
+                                                   Args&&... args) requires(I !=
+                                                                            0)
+        : rest(std::in_place_index<I - 1>, static_cast<Args&&>(args)...)
+    {}
+
+    Union(const Union&) requires(copy_construct&& trivially_copy_construct) =
+        default;
+    constexpr Union(const Union&) noexcept
+        requires(copy_construct && !trivially_copy_construct)
+    {}
+    Union(const Union&) requires(!copy_construct) = delete;
+
+    Union(Union&&) requires(move_construct&& trivially_move_construct) =
+        default;
+    constexpr Union(Union&&) noexcept
+        requires(move_construct && !trivially_move_construct)
+    {}
+    Union(Union&&) requires(!move_construct) = delete;
+
+    Union& operator                          =(const Union&) requires(
+        copy_assign&& trivially_copy_assign) = default;
+    constexpr Union& operator                =(const Union&) noexcept
+        requires(copy_assign && !trivially_copy_assign)
+    {
+        return *this;
+    }
+    Union& operator=(const Union&) requires(!copy_assign) = delete;
+
+    Union&
+    operator=(Union&&) requires(move_assign&& trivially_move_assign) = default;
+    constexpr Union& operator=(Union&&) noexcept
+        requires(move_assign && !trivially_move_assign)
+    {
+        return *this;
+    }
+    Union& operator=(Union&&) requires(!move_assign) = delete;
+
+    ~Union() requires(destructible&& trivially_destructible) = default;
+    ~Union() requires(destructible && !trivially_destructible)
+    {}
+    ~Union() requires(!destructible) = delete;
+
+    template<std::size_t I>
+    constexpr T& get_unchecked() & noexcept requires(I == 0)
+    {
+        return static_cast<T&>(first);
+    }
+
+    template<std::size_t I>
+    constexpr const T& get_unchecked() const& noexcept requires(I == 0)
+    {
+        return static_cast<const T&>(first);
+    }
+
+    template<std::size_t I>
+    constexpr T&& get_unchecked() && noexcept requires(I == 0)
+    {
+        return static_cast<T&&>(first);
+    }
+
+    template<std::size_t I>
+    constexpr const T&& get_unchecked() const&& noexcept requires(I == 0)
+    {
+        return static_cast<const T&&>(first);
+    }
+
+    template<std::size_t I>
+    constexpr auto get_unchecked() & noexcept
+        -> decltype(static_cast<rest_type&>(rest)
+                        .template get_unchecked<I - 1>()) requires(I != 0)
+    {
+        return static_cast<rest_type&>(rest).template get_unchecked<I - 1>();
+    }
+
+    template<std::size_t I>
+    constexpr auto get_unchecked() const& noexcept
+        -> decltype(static_cast<const rest_type&>(rest)
+                        .template get_unchecked<I - 1>()) requires(I != 0)
+    {
+        return static_cast<const rest_type&>(rest)
+            .template get_unchecked<I - 1>();
+    }
+
+    template<std::size_t I>
+    constexpr auto get_unchecked() && noexcept
+        -> decltype(static_cast<rest_type&&>(rest)
+                        .template get_unchecked<I - 1>()) requires(I != 0)
+    {
+        return static_cast<rest_type&&>(rest).template get_unchecked<I - 1>();
+    }
+
+    template<std::size_t I>
+    constexpr auto get_unchecked() const&& noexcept
+        -> decltype(static_cast<const rest_type&&>(rest)
+                        .template get_unchecked<I - 1>()) requires(I != 0)
+    {
+        return static_cast<const rest_type&&>(rest)
+            .template get_unchecked<I - 1>();
+    }
+};
+} // namespace ely
---
+++ test/union.cpp
@@ -0,0 +1,30 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <string>
+
+#include "ely/union.hpp"
+
+TEST_CASE("Union")
+{
+    auto empty = ely::Union<>{};
+
+    static_assert(std::is_empty_v<decltype(empty)>);
+
+    SUBCASE("trivial")
+    {
+        auto u = ely::Union<int, float, double>(std::in_place_index<1>, 5.f);
+
+        static_assert(std::is_trivial_v<decltype(u)>);
+        static_assert(std::is_trivially_copy_constructible_v<decltype(u)>);
+    }
+
+    SUBCASE("nontrivial")
+    {
+        auto u = ely::Union<int, std::string, float>(std::in_place_index<1>,
+                                                     "Hello");
+
+        static_assert(!std::is_trivial_v<decltype(u)>);
+        static_assert(std::is_copy_constructible_v<decltype(u)>);
+    }
+}
