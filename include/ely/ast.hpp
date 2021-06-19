e6a780128186c5c3c6768aaab00defb28a39a0b2
Frederik Engels <frederik.engels92@gmail.com>
2021-06-19 11:37:58 +0100

    define a few more ast nodes

--- include/ely/ast.hpp
+++ include/ely/ast.hpp
@@ -24,23 +24,38 @@ class Name
 
 class Define
 {
-    Name name;
-    Ast  init;
+    Name name_;
+    Ast  init_;
 };
 
 class Let
-{};
+{
+    std::vector<Var> bindings_;
+    std::vector<Ast> inits_;
+};
 
 class Fn
-{};
+{
+    ely::Vector<Var> args_;
+    Ast              body_;
+};
 
 class Value
 {};
 
 class Var
-{};
+{
+    Name name;
+};
+
+class Call
+{
+    ely::Vector<Ast> callee_and_args_;
+};
 
 class Ast
-{};
+{
+    ely::Variant<Var, Call, Value, Let> variant_;
+};
 } // namespace ast
 } // namespace ely
