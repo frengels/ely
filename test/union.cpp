8808b58f279ff882525f386297d95ceb236ecaed
Frederik Engels <frederik.engels92@gmail.com>
2021-06-27 21:10:14 +0100

    move variant tests out of union

--- test/union.cpp
+++ test/union.cpp
@@ -4,17 +4,9 @@
 #include <string>
 
 #include "ely/union.hpp"
-#include "ely/variant.hpp"
 
 class NoData
 {};
-
-class Inheriting : public ely::variant::Variant<int, float, std::string>
-{
-    using base = ely::variant::Variant<int, float, std::string>;
-
-    using base::base;
-};
 
 TEST_CASE("Union")
 {
@@ -32,7 +24,7 @@ TEST_CASE("Union")
     {
         auto u = ely::Union<bool, int>{};
 
-        REQUIRE_EQ(u.template get_unchecked<0>(), false);
+        REQUIRE_EQ(get_unchecked<0>(u), false);
     }
 
     SUBCASE("trivial")
@@ -51,72 +43,5 @@ TEST_CASE("Union")
         static_assert(!std::is_trivial_v<decltype(u)>);
         // copy must be allowed to make variant copy constructible as well
         static_assert(std::is_copy_constructible_v<decltype(u)>);
-    }
-}
-
-TEST_CASE("Variant")
-{
-    SUBCASE("single-entry")
-    {
-        // should be the same size as the underlying data
-        auto v = ely::variant::Variant<bool>{};
-        static_assert(sizeof(v) == sizeof(bool));
-    }
-
-    SUBCASE("construct")
-    {
-        auto v = ely::variant::Variant<int, float, std::string>{"hello world"};
-
-        REQUIRE_EQ(v.index(), 2); // require std::string
-
-        REQUIRE(visit(v, [](auto x) {
-            if constexpr (std::is_same_v<std::string, decltype(x)>)
-            {
-                return x == "hello world";
-            }
-            else
-            {
-                return false;
-            }
-        }));
-    }
-
-    SUBCASE("construct in_place_type")
-    {
-        auto v = ely::variant::Variant<int, float, bool>(
-            std::in_place_type<float>, 5);
-        REQUIRE_EQ(v.index(), 1);
-
-        SUBCASE("copy")
-        {
-            auto v2 = v;
-
-            REQUIRE_EQ(v2.index(), v.index());
-
-            v2 = std::move(v);
-        }
-    }
-
-    SUBCASE("nontrivial")
-    {
-        auto v1 = ely::variant::Variant<std::string>("Hello world");
-
-        REQUIRE_EQ(sizeof(v1), sizeof(std::string));
-
-        SUBCASE("copy")
-        {
-            auto v2 = v1;
-
-            REQUIRE_EQ(v2.index(), v1.index());
-
-            v1 = std::move(v2);
-        }
-    }
-
-    SUBCASE("inherit")
-    {
-        auto i = Inheriting("hello world");
-
-        REQUIRE_EQ(i.index(), 2);
     }
 }
---
+++ test/variant.cpp
@@ -0,0 +1,79 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <ely/variant.hpp>
+
+
+class Inheriting : public ely::variant::Variant<int, float, std::string>
+{
+    using base = ely::variant::Variant<int, float, std::string>;
+
+    using base::base;
+};
+
+TEST_CASE("Variant")
+{
+    SUBCASE("single-entry")
+    {
+        // should be the same size as the underlying data
+        auto v = ely::variant::Variant<bool>{};
+        static_assert(sizeof(v) == sizeof(bool));
+    }
+
+    SUBCASE("construct")
+    {
+        auto v = ely::variant::Variant<int, float, std::string>{"hello world"};
+
+        REQUIRE_EQ(v.index(), 2); // require std::string
+
+        REQUIRE(visit(v, [](auto x) {
+            if constexpr (std::is_same_v<std::string, decltype(x)>)
+            {
+                return x == "hello world";
+            }
+            else
+            {
+                return false;
+            }
+        }));
+    }
+
+    SUBCASE("construct in_place_type")
+    {
+        auto v = ely::variant::Variant<int, float, bool>(
+            std::in_place_type<float>, 5);
+        REQUIRE_EQ(v.index(), 1);
+
+        SUBCASE("copy")
+        {
+            auto v2 = v;
+
+            REQUIRE_EQ(v2.index(), v.index());
+
+            v2 = std::move(v);
+        }
+    }
+
+    SUBCASE("nontrivial")
+    {
+        auto v1 = ely::variant::Variant<std::string>("Hello world");
+
+        REQUIRE_EQ(sizeof(v1), sizeof(std::string));
+
+        SUBCASE("copy")
+        {
+            auto v2 = v1;
+
+            REQUIRE_EQ(v2.index(), v1.index());
+
+            v1 = std::move(v2);
+        }
+    }
+
+    SUBCASE("inherit")
+    {
+        auto i = Inheriting("hello world");
+
+        REQUIRE_EQ(i.index(), 2);
+    }
+}
