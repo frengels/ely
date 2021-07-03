93d213b43297902c0c792428fe4dd29c0dace65d
Frederik Engels <frederik.engels92@gmail.com>
2021-07-03 19:33:36 +0100

    make EBOPair truly EBO on all cases

--- include/ely/pair.hpp
+++ include/ely/pair.hpp
@@ -10,37 +10,93 @@ namespace ely
 {
 namespace detail
 {
-/// A box which can be tagged to prevent nasty inheritance issues
-template<int I, typename T, bool Empty = std::is_empty_v<T>>
+/// This box enables the use of EBO, the idea is when a type is not empty it
+/// will not be used as a base and when a type is empty it can be the base of
+/// this Box.
+/// In the case that both types T1 and T2 are equal and they are an empty type
+/// thus suitable for performing EBO, the second box will not inherit or contain
+/// any type, therefore both first() and second() sharing the same location in
+/// memory.
+template<int I,
+         typename T1,
+         typename T2,
+         bool Empty = std::is_empty_v<std::conditional_t<I == 0, T1, T2>>>
 class PairBox;
 
-template<int I, typename T>
-class PairBox<I, T, true> : private T
+template<int I, typename T1, typename T2>
+class PairBox<I, T1, T2, true> : private std::conditional_t<I == 0, T1, T2>
 {
+private:
+    using base_ = std::conditional_t<I == 0, T1, T2>;
+
+    using value_type = base_;
+
 public:
+    PairBox() = default;
+
     template<typename... Args>
     ELY_ALWAYS_INLINE explicit constexpr PairBox(Args&&... args)
-        : T(static_cast<Args&&>(args)...)
+        : base_(static_cast<Args&&>(args)...)
     {}
 
     template<typename Tuple, std::size_t... Is>
     ELY_ALWAYS_INLINE explicit constexpr PairBox(Tuple&& t,
                                                  std::index_sequence<Is...>)
-        : T(std::get<Is>(static_cast<Tuple&&>(t))...)
+        : base_(std::get<Is>(static_cast<Tuple&&>(t))...)
     {}
 
-    ELY_ALWAYS_INLINE constexpr const T& get() const& noexcept
+    ELY_ALWAYS_INLINE constexpr value_type& get() & noexcept
     {
-        return static_cast<T&>(*this);
+        return static_cast<value_type&>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr const value_type& get() const& noexcept
+    {
+        return static_cast<const value_type&>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr value_type&& get() && noexcept
+    {
+        return static_cast<value_type&&>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr const value_type&& get() const&& noexcept
+    {
+        return static_cast<const value_type&&>(*this);
     }
 };
 
-template<int I, typename T>
-class PairBox<I, T, false>
+template<typename T>
+class PairBox<1, T, T, true>
 {
-    T value;
+public:
+    PairBox() = default;
+
+    template<typename... Args>
+    explicit constexpr PairBox(Args&&... args) noexcept(
+        std::is_nothrow_constructible_v<T, Args...>)
+    {
+        T(static_cast<Args&&>(args)...);
+    }
+
+    template<typename Tuple, std::size_t... Is>
+    explicit constexpr PairBox(Tuple&& t, std::index_sequence<Is...>)
+    {
+        T(std::get<Is>(static_cast<Tuple&&>(t))...);
+    }
+};
+
+template<int I, typename T1, typename T2>
+class PairBox<I, T1, T2, false>
+{
+    using value_type = std::conditional_t<I == 0, T1, T2>;
+
+private:
+    value_type value;
 
 public:
+    PairBox() = default;
+
     template<typename... Args>
     ELY_ALWAYS_INLINE explicit constexpr PairBox(Args&&... args)
         : value(static_cast<Args&&>(args)...)
@@ -52,9 +108,24 @@ public:
         : value(std::get<Is>(static_cast<Tuple&&>(t))...)
     {}
 
-    ELY_ALWAYS_INLINE constexpr const T& get() const& noexcept
+    ELY_ALWAYS_INLINE constexpr value_type& get() & noexcept
     {
         return value;
+    }
+
+    ELY_ALWAYS_INLINE constexpr const value_type& get() const& noexcept
+    {
+        return value;
+    }
+
+    ELY_ALWAYS_INLINE constexpr value_type&& get() && noexcept
+    {
+        return static_cast<value_type&&>(value);
+    }
+
+    ELY_ALWAYS_INLINE constexpr const value_type&& get() const&& noexcept
+    {
+        return static_cast<const value_type&&>(value);
     }
 };
 } // namespace detail
@@ -63,28 +134,23 @@ public:
 /// types
 /// Very simple implementation for now
 template<typename T1, typename T2>
-class EBOPair : private detail::PairBox<0, T1>, private detail::PairBox<1, T2>
+class EBOPair : private detail::PairBox<0, T1, T2>,
+                private detail::PairBox<1, T1, T2>
 {
-private:
-    using first_box  = detail::PairBox<0, T1>;
-    using second_box = detail::PairBox<1, T2>;
-
 public:
     using first_type  = T1;
     using second_type = T2;
 
 private:
-    ELY_ALWAYS_INLINE constexpr const first_type& cfirst() const noexcept
-    {
-        return static_cast<const detail::PairBox<0, first_type>&>(*this).get();
-    }
+    using first_box  = detail::PairBox<0, T1, T2>;
+    using second_box = detail::PairBox<1, T1, T2>;
 
-    ELY_ALWAYS_INLINE constexpr const second_type& csecond() const noexcept
-    {
-        return static_cast<const detail::PairBox<1, second_type>&>(*this).get();
-    }
+    static constexpr bool same_type_and_empty =
+        std::is_same_v<T1, T2> && std::is_empty_v<T1>;
 
 public:
+    EBOPair() = default;
+
     ELY_ALWAYS_INLINE constexpr EBOPair(const first_type&  t1,
                                         const second_type& t2)
         : first_box(t1), second_box(t2)
@@ -105,44 +171,76 @@ public:
                      std::make_index_sequence<sizeof...(Args2)>{})
     {}
 
-    ELY_ALWAYS_INLINE constexpr const first_type& first() const& noexcept
-    {
-        return cfirst();
-    }
-
     ELY_ALWAYS_INLINE constexpr first_type& first() & noexcept
     {
-        return const_cast<first_type&>(cfirst());
+        return static_cast<first_box&>(*this).get();
     }
 
-    ELY_ALWAYS_INLINE constexpr const first_type&& first() const&& noexcept
+    ELY_ALWAYS_INLINE constexpr const first_type& first() const& noexcept
     {
-        return std::move(cfirst());
+        return static_cast<const first_box&>(*this).get();
     }
 
     ELY_ALWAYS_INLINE constexpr first_type&& first() && noexcept
     {
-        return std::move(first());
+        return static_cast<first_box&&>(*this).get();
     }
 
-    ELY_ALWAYS_INLINE constexpr const second_type& second() const& noexcept
+    ELY_ALWAYS_INLINE constexpr const first_type&& first() const&& noexcept
     {
-        return csecond();
+        return static_cast<const first_box&&>(*this).get();
     }
 
     ELY_ALWAYS_INLINE constexpr second_type& second() & noexcept
     {
-        return const_cast<second_type&>(csecond());
+        if constexpr (std::is_same_v<first_type, second_type> &&
+                      std::is_empty_v<first_type>)
+        {
+            return first();
+        }
+        else
+        {
+            return static_cast<second_box&>(*this).get();
+        }
+    }
+
+    ELY_ALWAYS_INLINE constexpr const second_type& second() const& noexcept
+    {
+        if constexpr (std::is_same_v<first_type, second_type> &&
+                      std::is_empty_v<first_type>)
+        {
+            return first();
+        }
+        else
+        {
+            return static_cast<const second_box&>(*this).get();
+        }
     }
 
     ELY_ALWAYS_INLINE constexpr second_type&& second() && noexcept
     {
-        return std::move(first());
+        if constexpr (std::is_same_v<first_type, second_type> &&
+                      std::is_empty_v<first_type>)
+        {
+            return std::move(*this).first();
+        }
+        else
+        {
+            return static_cast<second_box&&>(*this).get();
+        }
     }
 
     ELY_ALWAYS_INLINE constexpr const second_type&& second() const&& noexcept
     {
-        return std::move(cfirst());
+        if constexpr (std::is_same_v<first_type, second_type> &&
+                      std::is_empty_v<first_type>)
+        {
+            return std::move(*this).first();
+        }
+        else
+        {
+            return static_cast<const second_box&&>(*this).get();
+        }
     }
 };
 
