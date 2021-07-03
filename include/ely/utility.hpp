0a37ad8c39b98e708cfe2f2454f88bf57629c02f
Frederik Engels <frederik.engels92@gmail.com>
2021-07-03 14:01:39 +0100

    add utility counters for type uniqueness etc

--- include/ely/utility.hpp
+++ include/ely/utility.hpp
@@ -3,12 +3,114 @@
 #include "ely/defines.h"
 
 #include <memory>
+#include <optional>
 #include <type_traits>
 
 namespace ely
 {
 template<typename T>
 using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+namespace detail
+{
+struct type_index_helper
+{
+    constexpr std::size_t
+    operator()(std::initializer_list<bool> sames) const noexcept
+    {
+        std::size_t index = 0;
+
+        for (auto same : sames)
+        {
+            if (same)
+            {
+                return index;
+            }
+
+            ++index;
+        }
+
+        return index;
+    }
+};
+} // namespace detail
+
+template<typename T, typename... Ts>
+inline constexpr std::size_t
+    type_index_v = detail::type_index_helper{}({std::is_same_v<T, Ts>...});
+
+template<typename T, typename... Ts>
+struct type_index : std::integral_constant<std::size_t, type_index_v<T, Ts...>>
+{};
+
+namespace detail
+{
+struct type_count_helper
+{
+    constexpr std::size_t
+    operator()(std::initializer_list<bool> sames) const noexcept
+    {
+        std::size_t count = 0;
+
+        for (auto same : sames)
+        {
+            if (same)
+            {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
+} // namespace detail
+
+template<typename T, typename... Ts>
+inline constexpr std::size_t
+    type_count_v = detail::type_count_helper{}({std::is_same_v<T, Ts>...});
+
+template<typename T, typename... Ts>
+struct type_count : std::integral_constant<std::size_t, type_count_v<T, Ts...>>
+{};
+
+/// counts how often a type is repeated in a pack
+
+namespace detail
+{
+
+struct type_unique_helper
+{
+    constexpr bool
+    operator()(std::initializer_list<bool> sameness) const noexcept
+    {
+        bool found = false;
+
+        for (auto same : sameness)
+        {
+            if (same)
+            {
+                if (found)
+                {
+                    return false;
+                }
+
+                found = true;
+            }
+        }
+
+        return found;
+    }
+};
+
+} // namespace detail
+
+template<typename T, typename... Ts>
+inline constexpr std::size_t
+    type_unique_v = detail::type_unique_helper{}({std::is_same_v<T, Ts>...});
+
+template<typename T, typename... Ts>
+struct type_unique : std::bool_constant<type_unique_v<T, Ts...>>
+{};
 
 namespace detail
 {
