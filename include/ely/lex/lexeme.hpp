ca234584e185136e1378f861c2786c7c8b4bc8e7
Frederik Engels <frederik.engels92@gmail.com>
2021-08-06 02:55:16 +0100

    add not quite working conversions to lexeme subset

--- include/ely/lex/lexeme.hpp
+++ include/ely/lex/lexeme.hpp
@@ -81,121 +81,239 @@ struct NewlineLf
 struct NewlineCrlf
 {};
 
-using variant_type = ely::Variant<Eof,
-                                  LParen,
-                                  RParen,
-                                  LBracket,
-                                  RBracket,
-                                  LBrace,
-                                  RBrace,
-                                  Identifier,
-                                  IntLit,
+template<typename... Ts>
+class LexemeBase : public ely::Variant<Ts...>
+{
+private:
+    using base_ = ely::Variant<Ts...>;
+
+public:
+    using base_::base_;
+
+    ELY_ALWAYS_INLINE explicit constexpr operator bool() const noexcept
+    {
+        return !ely::holds_alternative<Eof>(*this);
+    }
+};
+
+using All = LexemeBase<Eof,
+                       LParen,
+                       RParen,
+                       LBracket,
+                       RBracket,
+                       LBrace,
+                       RBrace,
+                       Identifier,
+                       IntLit,
+                       FloatLit,
+                       CharLit,
+                       StringLit,
+                       KeywordLit,
+                       BoolLit,
+                       Colon,
+                       Quote,
+                       SyntaxQuote,
+                       At,
+                       Unquote,
+                       SyntaxUnquote,
+                       UnquoteSplicing,
+                       SyntaxUnquoteSplicing,
+                       Exclamation,
+                       Question,
+                       Ampersand,
+                       QuasiQuote,
+                       QuasiSyntax,
+                       UnterminatedStringLit,
+                       InvalidNumberSign,
+                       Whitespace,
+                       Tab,
+                       Comment,
+                       NewlineCr,
+                       NewlineLf,
+                       NewlineCrlf>;
+
+class TrailingAtmosphere : public LexemeBase<Whitespace, Tab, Comment>
+{
+    using base_ = LexemeBase<Whitespace, Tab, Comment>;
+
+public:
+    using base_::base_;
+};
+
+class LeadingAtmosphere : public LexemeBase<Whitespace,
+                                            Tab,
+                                            Comment,
+                                            NewlineCr,
+                                            NewlineLf,
+                                            NewlineCrlf>
+{
+    using base_ =
+        LexemeBase<Whitespace, Tab, Comment, NewlineCr, NewlineLf, NewlineCrlf>;
+
+public:
+    using base_::base_;
+};
+
+using Atmosphere = LeadingAtmosphere;
+
+class Literal : public LexemeBase<IntLit,
                                   FloatLit,
                                   CharLit,
                                   StringLit,
                                   KeywordLit,
                                   BoolLit,
-                                  Colon,
-                                  Quote,
-                                  SyntaxQuote,
-                                  At,
-                                  Unquote,
-                                  SyntaxUnquote,
-                                  UnquoteSplicing,
-                                  SyntaxUnquoteSplicing,
-                                  Exclamation,
-                                  Question,
-                                  Ampersand,
-                                  QuasiQuote,
-                                  QuasiSyntax,
-                                  UnterminatedStringLit,
-                                  InvalidNumberSign,
-                                  Whitespace,
-                                  Tab,
-                                  Comment,
-                                  NewlineCr,
-                                  NewlineLf,
-                                  NewlineCrlf>;
+                                  UnterminatedStringLit>
+{
+    using base_ = LexemeBase<IntLit,
+                             FloatLit,
+                             CharLit,
+                             StringLit,
+                             KeywordLit,
+                             BoolLit,
+                             UnterminatedStringLit>;
+
+public:
+    using base_::base_;
+};
+
+class PrefixAbbrev : public LexemeBase<Quote,
+                                       SyntaxQuote,
+                                       At,
+                                       Unquote,
+                                       SyntaxUnquote,
+                                       UnquoteSplicing,
+                                       SyntaxUnquoteSplicing,
+                                       Exclamation,
+                                       Question,
+                                       Ampersand,
+                                       QuasiQuote,
+                                       QuasiSyntax>
+{
+    using base_ = LexemeBase<Quote,
+                             SyntaxQuote,
+                             At,
+                             Unquote,
+                             SyntaxUnquote,
+                             UnquoteSplicing,
+                             SyntaxUnquoteSplicing,
+                             Exclamation,
+                             Question,
+                             Ampersand,
+                             QuasiQuote,
+                             QuasiSyntax>;
+
+public:
+    using base_::base_;
+};
+
+using InfixAbbrev = Colon;
+
+class Abbrev : public LexemeBase<Quote,
+                                 SyntaxQuote,
+                                 At,
+                                 Unquote,
+                                 SyntaxUnquote,
+                                 UnquoteSplicing,
+                                 SyntaxUnquoteSplicing,
+                                 Exclamation,
+                                 Question,
+                                 Ampersand,
+                                 QuasiQuote,
+                                 QuasiSyntax,
+                                 Colon>
+{
+    using base_ = LexemeBase<Quote,
+                             SyntaxQuote,
+                             At,
+                             Unquote,
+                             SyntaxUnquote,
+                             UnquoteSplicing,
+                             SyntaxUnquoteSplicing,
+                             Exclamation,
+                             Question,
+                             Ampersand,
+                             QuasiQuote,
+                             QuasiSyntax,
+                             Colon>;
+
+public:
+    using base_::base_;
+};
 } // namespace lexeme
 
-class LexemeKind : public lexeme::variant_type
+namespace detail
 {
-    using base_ = lexeme::variant_type;
+template<typename Variant, typename T>
+inline constexpr bool variant_contains_v =
+    typename Variant::value_types::template apply_all<
+        ely::is_same_mf<T>::template invoke>::value;
+
+template<typename VRes, typename VT>
+ELY_ALWAYS_INLINE constexpr VRes convert_variant_unchecked(VT&& v)
+{
+    ely::visit(
+        [](auto&& x) -> VRes {
+            // this will only work on non cvref qualified members
+            using ty = ely::remove_cvref_t<decltype(x)>;
+            if constexpr (variant_contains_v<VRes, ty>)
+            {
+                return VRes{static_cast<decltype(x)&&>(x)};
+            }
+            else
+            {
+                __builtin_unreachable();
+            }
+        },
+        static_cast<VT&&>(v));
+}
+
+template<typename OtherV, typename ThisV>
+ELY_ALWAYS_INLINE constexpr bool
+holds_one_of_other_alternatives(const ThisV& v) noexcept
+{
+    return ely::visit<bool>(
+        [](auto&& x) {
+            using ty = ely::remove_cvref_t<decltype(x)>;
+
+            return typename OtherV::value_types::template apply_all<
+                ely::is_same_mf<ty>::template invoke>::value;
+        },
+        v);
+}
+} // namespace detail
+
+class LexemeKind : public lexeme::All
+{
+    using base_ = lexeme::All;
 
 public:
     using base_::base_;
 
     ELY_ALWAYS_INLINE constexpr bool is_trailing_atmosphere() const noexcept
     {
-        return ely::visit<bool>(
-            [](const auto& x) {
-                using ty = ely::remove_cvref_t<decltype(x)>;
-
-                return ely::is_same_one_of_v<ty,
-                                             lexeme::Whitespace,
-                                             lexeme::Tab,
-                                             lexeme::Comment>;
-            },
-            *this);
+        return detail::holds_one_of_other_alternatives<
+            lexeme::TrailingAtmosphere>(*this);
     }
 
     ELY_ALWAYS_INLINE constexpr bool is_leading_atmosphere() const noexcept
     {
-        return ely::visit<bool>(
-                   [](const auto& x) {
-                       using ty = ely::remove_cvref_t<decltype(x)>;
-                       return ely::is_same_one_of_v<ty,
-                                                    lexeme::NewlineCr,
-                                                    lexeme::NewlineLf,
-                                                    lexeme::NewlineCrlf>;
-                   },
-                   *this) ||
-               is_trailing_atmosphere();
+        return detail::holds_one_of_other_alternatives<
+            lexeme::LeadingAtmosphere>(*this);
     }
 
     ELY_ALWAYS_INLINE constexpr bool is_atmosphere() const noexcept
     {
-        return is_trailing_atmosphere();
+        return is_leading_atmosphere();
     }
 
     ELY_ALWAYS_INLINE constexpr bool is_literal() const noexcept
     {
-        return ely::visit<bool>(
-            [](const auto& x) {
-                using ty = ely::remove_cvref_t<decltype(x)>;
-
-                return ely::is_same_one_of_v<ty,
-                                             lexeme::IntLit,
-                                             lexeme::FloatLit,
-                                             lexeme::CharLit,
-                                             lexeme::StringLit,
-                                             lexeme::KeywordLit,
-                                             lexeme::BoolLit,
-                                             lexeme::UnterminatedStringLit>;
-            },
-            *this);
+        return detail::holds_one_of_other_alternatives<lexeme::Literal>(*this);
     }
 
     ELY_ALWAYS_INLINE constexpr bool is_prefix_abbrev() const noexcept
     {
-        return ely::visit<bool>(
-            [](const auto& x) {
-                using ty = ely::remove_cvref_t<decltype(x)>;
-
-                return ely::is_same_one_of_v<ty,
-                                             lexeme::Quote,
-                                             lexeme::SyntaxQuote,
-                                             lexeme::At,
-                                             lexeme::Unquote,
-                                             lexeme::SyntaxUnquote,
-                                             lexeme::UnquoteSplicing,
-                                             lexeme::SyntaxUnquoteSplicing,
-                                             lexeme::Exclamation,
-                                             lexeme::Question,
-                                             lexeme::Ampersand,
-                                             lexeme::QuasiQuote,
-                                             lexeme::QuasiSyntax>;
-            },
+        return detail::holds_one_of_other_alternatives<lexeme::PrefixAbbrev>(
             *this);
     }
 
@@ -211,36 +329,87 @@ public:
 
     ELY_ALWAYS_INLINE constexpr bool is_poison() const noexcept
     {
-        return ely::visit<bool>(
-            [](const auto& x) {
-                using ty = ely::remove_cvref_t<decltype(x)>;
-                return ely::is_same_one_of_v<ty,
-                                             lexeme::UnterminatedStringLit,
-                                             lexeme::InvalidNumberSign>;
-            },
-            *this);
+        return detail::holds_one_of_other_alternatives<lexeme::Poison>(*this);
     }
 
     ELY_ALWAYS_INLINE constexpr bool is_eof() const noexcept
     {
         return ely::holds_alternative<lexeme::Eof>(*this);
     }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::TrailingAtmosphere
+    as_trailing_atmosphere_unchecked() const noexcept
+    {
+            ELY_ASSERT(is_trailing_atmosphere(), "expected trailing
+       atmosphere"); return
+       detail::convert_variant_unchecked<lexeme::TrailingAtmosphere>( *this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::LeadingAtmosphere
+    as_leading_atmosphere_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_leading_atmosphere(), "expected leading atmosphere");
+        return detail::convert_variant_unchecked<lexeme::LeadingAtmosphere>(
+            *this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::Atmosphere
+    as_atmosphere_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_atmosphere(), "expected atmosphere");
+        return detail::convert_variant_unchecked<lexeme::Atmosphere>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::Literal
+    as_literal_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_literal(), "expected literal");
+        return detail::convert_variant_unchecked<lexeme::Literal>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::PrefixAbbrev
+    as_prefix_abbrev_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_prefix_abbrev(), "expected prefix abbreviation");
+        return detail::convert_variant_unchecked<lexeme::PrefixAbbrev>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::InfixAbbrev
+    as_infix_abbrev_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_infix_abbrev(), "expected infix abbreviation");
+        return {};
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::Abbrev
+    as_abbrev_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_abbrev(), "expected abbreviation");
+        return detail::convert_variant_unchecked<lexeme::Abbrev>(*this);
+    }
+
+    ELY_ALWAYS_INLINE constexpr lexeme::Eof as_eof_unchecked() const noexcept
+    {
+        ELY_ASSERT(is_eof(), "expected EOF");
+        return {};
+    }
 };
 
-template<typename I>
+template<typename I, typename L = LexemeKind>
 struct Lexeme
 {
 public:
-    using iterator  = I;
-    using size_type = uint32_t;
+    using iterator    = I;
+    using size_type   = uint32_t;
+    using lexeme_type = L;
 
 public:
     LexemeSpan<I> span;
-    LexemeKind    kind{lexeme::Eof{}};
+    L             kind{};
 
     ELY_ALWAYS_INLINE explicit constexpr operator bool() const noexcept
     {
-        return !kind.is_eof();
+        return kind;
     }
 };
 } // namespace ely
