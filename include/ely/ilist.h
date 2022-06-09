39ad25a12adf6b87201ffc4965d4257a8b2850a0
Frederik Engels <frederik.engels92@gmail.com>
2022-06-09 12:07:40 +0100

    rename list to ilist

--- include/ely/ilist.h
+++ include/ely/ilist.h
@@ -14,36 +14,36 @@ extern "C" {
 #define ELY_CONTAINER_OF(ptr, sample, member)                                  \
     (__typeof__(sample)) ((char*) (ptr) -offsetof(__typeof__(*sample), member))
 
-typedef struct ely_list
+typedef struct ely_ilist
 {
-    struct ely_list* prev;
-    struct ely_list* next;
-} ely_list;
+    struct ely_ilist* prev;
+    struct ely_ilist* next;
+} ely_ilist;
 
-ELY_EXPORT void     ely_list_init(ely_list* list);
-ELY_EXPORT void     ely_list_insert(ely_list* list, ely_list* element);
-ELY_EXPORT void     ely_list_remove(ely_list* element);
-ELY_EXPORT uint32_t ely_list_length(const ely_list* list);
-ELY_EXPORT bool     ely_list_empty(const ely_list* list);
-ELY_EXPORT void     ely_list_insert_list(ely_list* list, ely_list* other);
+ELY_EXPORT void     ely_ilist_init(ely_ilist* list);
+ELY_EXPORT void     ely_ilist_insert(ely_ilist* list, ely_ilist* element);
+ELY_EXPORT void     ely_ilist_remove(ely_ilist* element);
+ELY_EXPORT uint32_t ely_ilist_length(const ely_ilist* list);
+ELY_EXPORT bool     ely_ilist_empty(const ely_ilist* list);
+ELY_EXPORT void     ely_ilist_insert_list(ely_ilist* list, ely_ilist* other);
 
-#define ELY_LIST_FOR_EACH(pos, head, member)                                   \
+#define ELY_ILIST_FOR_EACH(pos, head, member)                                  \
     for (pos = ELY_CONTAINER_OF((head)->next, pos, member);                    \
          &pos->member != (head);                                               \
          pos = ELY_CONTAINER_OF(pos->member.next, pos, member))
 
-#define ELY_LIST_FOR_EACH_SAFE(pos, tmp, head, member)                         \
+#define ELY_ILIST_FOR_EACH_SAFE(pos, tmp, head, member)                        \
     for (pos = ELY_CONTAINER_OF((head)->next, pos, member),                    \
         tmp  = ELY_CONTAINER_OF((pos)->member.next, tmp, member);              \
          &pos->member != (head);                                               \
          pos = tmp, tmp = ELY_CONTAINER_OF(pos->member.next, tmp, member))
 
-#define ELY_LIST_FOR_EACH_REVERSE(pos, head, member)                           \
+#define ELY_ILIST_FOR_EACH_REVERSE(pos, head, member)                          \
     for (pos = ELY_CONTAINER_OF((head)->prev, pos, member);                    \
          &pos->member != (head);                                               \
          pos = ELY_CONTAINER_OF(pos->member.prev, pos, member))
 
-#define ELY_LIST_FOR_EACH_REVERSE_SAFE(pos, tmp, head, member)                 \
+#define ELY_ILIST_FOR_EACH_REVERSE_SAFE(pos, tmp, head, member)                \
     for (pos = ELY_CONTAINER_OF((head)->prev, pos, member),                    \
         tmp  = ELY_CONTAINER_OF((pos)->member.prev, tmp, member);              \
          &pos->member != (head);                                               \
--- src/ilist.c
+++ src/ilist.c
@@ -1,12 +1,12 @@
-#include "ely/list.h"
+#include "ely/ilist.h"
 
-void ely_list_init(ely_list* list)
+void ely_ilist_init(ely_ilist* list)
 {
     list->prev = list;
     list->next = list;
 }
 
-void ely_list_insert(ely_list* list, ely_list* element)
+void ely_ilist_insert(ely_ilist* list, ely_ilist* element)
 {
     element->prev       = list;
     element->next       = list->next;
@@ -14,7 +14,7 @@ void ely_list_insert(ely_list* list, ely_list* element)
     element->next->prev = element;
 }
 
-void ely_list_remove(ely_list* element)
+void ely_ilist_remove(ely_ilist* element)
 {
     element->prev->next = element->next;
     element->next->prev = element->prev;
@@ -22,10 +22,10 @@ void ely_list_remove(ely_list* element)
     element->prev       = NULL;
 }
 
-uint32_t ely_list_length(const ely_list* list)
+uint32_t ely_ilist_length(const ely_ilist* list)
 {
-    ely_list* e   = list->next;
-    uint32_t  len = 0;
+    ely_ilist* e   = list->next;
+    uint32_t   len = 0;
 
     while (e != list)
     {
@@ -36,14 +36,14 @@ uint32_t ely_list_length(const ely_list* list)
     return len;
 }
 
-bool ely_list_empty(const ely_list* list)
+bool ely_ilist_empty(const ely_ilist* list)
 {
     return list->next == list;
 }
 
-void ely_list_insert_list(ely_list* list, ely_list* other)
+void ely_ilist_insert_list(ely_ilist* list, ely_ilist* other)
 {
-    if (ely_list_empty(other))
+    if (ely_ilist_empty(other))
     {
         return;
     }
