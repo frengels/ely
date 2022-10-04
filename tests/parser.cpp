d7cdd49a7ff7d43f381d99b0cbc1e13e034cb3b1
Frederik Engels <frederik.engels92@gmail.com>
2022-10-04 13:11:38 +0100

    add test for parsing

--- tests/parser.cpp
+++ tests/parser.cpp
@@ -11,4 +11,21 @@ TEST_CASE("parser") {
   auto p = mli::parser{src};
 
   auto tree = p.parse(alloc);
+
+  auto l = tree.sexps()[0];
+  auto num = tree.sexps()[1];
+
+  CHECK(l->isa<mli::list>());
+  CHECK(l->pos() == mli::source_position{1, 1});
+  CHECK(num->isa<mli::decimal_literal>());
+  CHECK(num->pos() == mli::source_position{2, 1});
+
+  mli::arena_ptr<mli::list> l1 = l->get_if<mli::list>();
+  CHECK(l1);
+  CHECK(l1->sexps()[0]->isa<mli::identifier>());
+  CHECK(l1->sexps()[1]->isa<mli::identifier>());
+
+  mli::arena_ptr<mli::decimal_literal> dec =
+      num->get_if<mli::decimal_literal>();
+  CHECK(dec);
 }
