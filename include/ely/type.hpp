2a255c02429c1937038aa7be6974e6e786235646
Frederik Engels <frederik.engels92@gmail.com>
2022-10-24 18:53:23 +0100

    add more get_type_ functions

--- include/ely/type.hpp
+++ include/ely/type.hpp
@@ -11,9 +11,49 @@
 
 namespace ely
 {
+constexpr ely_type get_type_poison()
+{
+    return {ELY_TYPE_POISON, {}};
+}
+
 constexpr ely_type get_type_generic()
 {
     return {ELY_TYPE_GENERIC, {}};
+}
+
+constexpr ely_type get_type_void()
+{
+    return {ELY_TYPE_VOID, {}};
+}
+
+constexpr ely_type get_type_u32()
+{
+    return {ELY_TYPE_U32, {}};
+}
+
+constexpr ely_type get_type_u64()
+{
+    return {ELY_TYPE_U64, {}};
+}
+
+constexpr ely_type get_type_s32()
+{
+    return {ELY_TYPE_S32, {}};
+}
+
+constexpr ely_type get_type_s64()
+{
+    return {ELY_TYPE_S64, {}};
+}
+
+constexpr ely_type get_type_f32()
+{
+    return {ELY_TYPE_F32, {}};
+}
+
+constexpr ely_type get_type_f64()
+{
+    return {ELY_TYPE_F64, {}};
 }
 
 template<typename T>
@@ -21,37 +61,37 @@ constexpr ely_type get_type()
 {
     if constexpr (std::is_same_v<void, T>)
     {
-        return {ELY_TYPE_VOID, {}};
+        return get_type_void();
     }
     else if constexpr (std::is_same_v<std::uint32_t, T>)
     {
-        return {ELY_TYPE_U32, {}};
+        return get_type_u32();
     }
     else if constexpr (std::is_same_v<std::uint64_t, T>)
     {
-        return {ELY_TYPE_U64, {}};
+        return get_type_u64();
     }
     else if constexpr (std::is_same_v<std::int32_t, T>)
     {
-        return {ELY_TYPE_S32, {}};
+        return get_type_s32();
     }
     else if constexpr (std::is_same_v<std::int64_t, T>)
     {
-        return {ELY_TYPE_S64, {}};
+        return get_type_s64();
     }
     else if constexpr (std::is_same_v<float, T>)
     {
-        return {ELY_TYPE_F32, {}};
+        return get_type_f32();
     }
     else if constexpr (std::is_same_v<double, T>)
     {
-        return {ELY_TYPE_F64, {}};
+        return get_type_f64();
     }
     else
     {
         static_assert(!std::is_same_v<int, int>,
                       "cannot get ely type for c++ type");
-        return {ELY_TYPE_POISON, {}};
+        return get_type_poison();
     }
 }
 
