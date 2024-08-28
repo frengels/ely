abf34658db967a4a7e53408fbf22d81f1b1a9e94
Frederik Engels <frederik.engels92@gmail.com>
2024-08-28 22:49:46 +0100

    read files using stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,46 @@
-#include "lex.hpp"
+#include <cstdio>
 
-int main(int argc, char** argv) {}
+#include <cinttypes>
+#include <cstdlib>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include "lexer.hpp"
+#include "stream.hpp"
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::fputs("ely: missing input\n", stderr);
+    return EXIT_FAILURE;
+  }
+
+  // open file
+  auto fdin = open(argv[1], O_RDONLY);
+  if (fdin < 0) {
+    std::fprintf(stderr, "can't open %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  // get file length
+  struct stat statbuf;
+  if (fstat(fdin, &statbuf) < 0) {
+    perror("fstat");
+    return EXIT_FAILURE;
+  }
+
+  constexpr auto buffer_size = 64 * 1024;
+  char* buffer = new char[buffer_size];
+
+  ely::file_stream char_stream{argv[1], buffer, buffer_size};
+
+  // print lexical tokens
+  auto lex = ely::lexer<ely::file_stream>{std::move(char_stream)};
+  std::string tok_buffer;
+  std::fputs("[\n", stdout);
+  for (auto tok = lex.next(tok_buffer); tok.kind != ely::token_kind::eof;
+       tok_buffer.clear(), tok = lex.next(tok_buffer)) {
+    std::fprintf(stdout, "  (token :length %" PRIu16 ")\n", tok.len);
+  }
+  std::fputs("]\n", stdout);
+}
