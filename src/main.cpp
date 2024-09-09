c77d72dd6eb8d4b46f65e573c5c7fabd73153fe8
Frederik Engels <frederik.engels92@gmail.com>
2024-09-09 22:55:43 +0100

    make in out file opening reusable

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,66 @@
 #include "lexer.hpp"
 #include "stream.hpp"
 
+std::FILE* open_output_file(const char* outfile) {
+  if (std::strcmp("-", outfile) == 0) {
+    return stdout;
+  }
+
+  return std::fopen(outfile, "wb");
+}
+
+struct in_out_files {
+  std::FILE* out_file;
+  std::vector<std::FILE*> input_files;
+};
+
+in_out_files parse_in_out(std::span<char*> args) {
+  const char* outfile = nullptr;
+  std::vector<const char*> inputfiles;
+
+  for (unsigned i = 0; i != args.size(); ++i) {
+    if (std::strcmp("-o", args[i]) == 0) {
+      ++i;
+      outfile = args[i];
+    } else {
+      inputfiles.push_back(args[i]);
+    }
+  }
+
+  if (!outfile) {
+    std::fputs("ely: error: no output file\n", stderr);
+    return {};
+  }
+
+  std::FILE* out = open_output_file(outfile);
+
+  if (!out) {
+    std::fprintf(stderr, "ely: error: failed to open \"%s\" for writing\n",
+                 outfile);
+    return {};
+  }
+
+  std::vector<std::FILE*> input_streams;
+
+  for (const char* file : inputfiles) {
+    std::FILE* f = std::fopen(file, "rb");
+    if (!f) {
+      std::fprintf(stderr, "ely: error: failed to open \"%s\" for reading\n",
+                   file);
+      continue;
+    }
+
+    input_streams.push_back(f);
+  }
+
+  if (input_streams.empty()) {
+    std::fputs("ely: error: no input files\n", stderr);
+    return {};
+  }
+
+  return {out, std::move(input_streams)};
+}
+
 int execute_lex(std::span<char*> args);
 int execute_parse(std::span<char*> args);
 
@@ -35,54 +95,18 @@ int main(int argc, char** argv) {
   }
 }
 
-std::FILE* open_output_file(const char* outfile) {
-  if (std::strcmp("-", outfile) == 0) {
-    return stdout;
-  }
-
-  return std::fopen(outfile, "wb");
-}
-
 int execute_lex(std::span<char*> args) {
-  const char* outfile = nullptr;
-  std::vector<const char*> inputfiles;
-
-  for (unsigned i = 0; i != args.size(); ++i) {
-    if (std::strcmp("-o", args[i]) == 0) {
-      ++i;
-      outfile = args[i];
-    } else {
-      inputfiles.push_back(args[i]);
-    }
-  }
-
-  if (!outfile) {
-    std::fputs("ely: error: no output file\n", stderr);
+  auto in_out = parse_in_out(args);
+  if (!in_out.out_file) {
     return EXIT_FAILURE;
   }
 
-  auto out = open_output_file(outfile);
-
-  if (!out) {
-    std::fprintf(stderr, "ely: error: failed to open \"%s\" for writing\n",
-                 outfile);
-    return EXIT_FAILURE;
-  }
-
-  if (inputfiles.empty()) {
-    std::fputs("ely: error: no input files\n", stderr);
-    return EXIT_FAILURE;
-  }
+  std::FILE* out = in_out.out_file;
 
   constexpr auto buffer_size = 64 * 1024;
   char buffer[buffer_size];
-  for (const char* input_file : inputfiles) {
+  for (std::FILE* input_file : in_out.input_files) {
     ely::file_stream stream{input_file, buffer, buffer_size};
-
-    if (!stream) {
-      std::fprintf(stderr, "ely: error: failed to open \"%s\"\n", input_file);
-      return EXIT_FAILURE;
-    }
 
     auto lex = ely::lexer<ely::file_stream>{std::move(stream)};
     std::fputs("[\n", out);
