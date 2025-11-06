1e58273682442f9e8bf3578e65b1bd2c9f8d0c53
Frederik Engels <frederik.engels92@gmail.com>
2025-11-06 08:43:28 +0000

    simplify union_storage with a single class

--- include/ely/util/union_storage.hpp
+++ include/ely/util/union_storage.hpp
@@ -8,172 +8,141 @@
 namespace ely {
 
 namespace detail {
-template <bool B, typename... Ts> union union_storage_impl;
-}
 
-// template <typename... Ts> union union_storage {};
+template <typename T0 = char, typename T1 = T0, typename T2 = T0,
+          typename T3 = T0, typename... Rest>
+union union_storage_impl {
+private:
+  inline static constexpr bool has_tail = sizeof...(Rest) > 0;
+  using tail_type =
+      std::conditional_t<has_tail, union_storage_impl<Rest...>, T3>;
+
+private:
+  T0 t0_;
+  T1 t1_;
+  T2 t2_;
+  T3 t3_;
+  tail_type rest_;
+
+public:
+  template <typename... Args>
+  explicit constexpr union_storage_impl(std::in_place_index_t<0>,
+                                        Args&&... args)
+      : t0_(static_cast<Args&&>(args)...) {}
+
+  template <typename... Args>
+  explicit constexpr union_storage_impl(std::in_place_index_t<1>,
+                                        Args&&... args)
+      : t1_(static_cast<Args&&>(args)...) {}
+
+  template <typename... Args>
+  explicit constexpr union_storage_impl(std::in_place_index_t<2>,
+                                        Args&&... args)
+      : t2_(static_cast<Args&&>(args)...) {}
+
+  template <typename... Args>
+  explicit constexpr union_storage_impl(std::in_place_index_t<3>,
+                                        Args&&... args)
+      : t3_(static_cast<Args&&>(args)...) {}
+
+  template <std::size_t I, typename... Args>
+  explicit constexpr union_storage_impl(std::in_place_index_t<I>,
+                                        Args&&... args)
+      : rest_(std::in_place_index<I - 4>, static_cast<Args&&>(args)...) {}
+
+  constexpr ~union_storage_impl()
+    requires(!(std::is_trivially_destructible_v<T0> &&
+               std::is_trivially_destructible_v<T1> &&
+               std::is_trivially_destructible_v<T2> &&
+               std::is_trivially_destructible_v<T3> &&
+               std::is_trivially_destructible_v<tail_type>))
+  {}
+
+  ~union_storage_impl()
+    requires(std::is_trivially_destructible_v<T0> &&
+             std::is_trivially_destructible_v<T1> &&
+             std::is_trivially_destructible_v<T2> &&
+             std::is_trivially_destructible_v<T3> &&
+             std::is_trivially_destructible_v<tail_type>)
+  = default;
+
+  template <std::size_t I, typename... Args>
+  constexpr void emplace(std::in_place_index_t<I> i, Args&&... args) {
+    std::construct_at(std::addressof(get(i)), static_cast<Args&&>(args)...);
+  }
+
+  template <std::size_t I>
+  constexpr void destroy(std::in_place_index_t<I> i) noexcept {
+    std::destroy_at(std::addressof(get(i)));
+  }
+
+  template <std::size_t I>
+  constexpr const auto& get(std::in_place_index_t<I>) const& noexcept {
+    if constexpr (I == 0) {
+      return t0_;
+    } else if constexpr (I == 1) {
+      return t1_;
+    } else if constexpr (I == 2) {
+      return t2_;
+    } else if constexpr (I == 3) {
+      return t3_;
+    } else {
+      return rest_.get(std::in_place_index<I - 4>);
+    }
+  }
+
+  template <std::size_t I>
+  constexpr auto& get(std::in_place_index_t<I>) & noexcept {
+    if constexpr (I == 0) {
+      return t0_;
+    } else if constexpr (I == 1) {
+      return t1_;
+    } else if constexpr (I == 2) {
+      return t2_;
+    } else if constexpr (I == 3) {
+      return t3_;
+    } else {
+      return rest_.get(std::in_place_index<I - 4>);
+    }
+  }
+
+  template <std::size_t I>
+  constexpr auto&& get(std::in_place_index_t<I>) && noexcept {
+    if constexpr (I == 0) {
+      return static_cast<T0&&>(t0_);
+    } else if constexpr (I == 1) {
+      return static_cast<T1&&>(t1_);
+    } else if constexpr (I == 2) {
+      return static_cast<T2&&>(t2_);
+    } else if constexpr (I == 3) {
+      return static_cast<T3&&>(t3_);
+    } else {
+      return static_cast<tail_type&&>(rest_).get(std::in_place_index<I - 4>);
+    }
+  }
+
+  template <std::size_t I>
+  constexpr auto&& get(std::in_place_index_t<I>) const&& noexcept {
+    if constexpr (I == 0) {
+      return static_cast<const T0&&>(t0_);
+    } else if constexpr (I == 1) {
+      return static_cast<const T1&&>(t1_);
+    } else if constexpr (I == 2) {
+      return static_cast<const T2&&>(t2_);
+    } else if constexpr (I == 3) {
+      return static_cast<const T3&&>(t3_);
+    } else {
+      return static_cast<const tail_type&&>(rest_).get(
+          std::in_place_index<I - 4>);
+    }
+  }
+};
+} // namespace detail
 
 template <typename... Ts>
-using union_storage =
-    detail::union_storage_impl<(std::is_trivially_destructible_v<Ts> && ...),
-                               Ts...>;
+using union_storage = detail::union_storage_impl<Ts...>;
 
 namespace detail {
-template <bool B> union union_storage_impl<B> {};
-
-template <typename T, typename... Ts>
-union union_storage_impl<false, T, Ts...> {
-  T first_;
-  union_storage<Ts...> rest_;
-
-  template <typename... Args>
-  constexpr union_storage_impl(std::in_place_index_t<0>, Args&&... args)
-      : first_(static_cast<Args&&>(args)...) {}
-
-  template <std::size_t I, typename... Args>
-  constexpr union_storage_impl(std::in_place_index_t<I>, Args&&... args)
-      : rest_(std::in_place_index<I - 1>, static_cast<Args&&>(args)...) {}
-
-  constexpr ~union_storage_impl() noexcept {}
-
-  template <typename... Args>
-  constexpr void emplace(std::in_place_index_t<0>, Args&&... args) {
-    std::construct_at(&first_, static_cast<Args&&>(args)...);
-  }
-
-  template <std::size_t I, typename... Args>
-  constexpr void emplace(std::in_place_index_t<I>, Args&&... args) {
-    return rest_.emplace(std::in_place_index<I - 1>,
-                         static_cast<Args&&>(args)...);
-  }
-
-  constexpr void destroy(std::in_place_index_t<0>) noexcept {
-    if constexpr (!std::is_trivially_destructible_v<T>) {
-      std::destroy_at(&first_);
-    }
-  }
-
-  template <std::size_t I>
-  constexpr void destroy(std::in_place_index_t<I>) noexcept {
-    return rest_.destroy(std::in_place_index<I - 1>);
-  }
-
-  constexpr T& get(std::in_place_index_t<0>) & noexcept { return first_; }
-  constexpr const T& get(std::in_place_index_t<0>) const& noexcept {
-    return first_;
-  }
-  constexpr T&& get(std::in_place_index_t<0>) && noexcept {
-    return static_cast<T&&>(first_);
-  }
-  constexpr const T&& get(std::in_place_index_t<0>) const&& noexcept {
-    return static_cast<const T&&>(first_);
-  }
-
-  template <std::size_t I>
-  constexpr auto& get(std::in_place_index_t<I>) & noexcept {
-    return rest_.get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr const auto& get(std::in_place_index_t<I>) const& noexcept {
-    return rest_.get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr auto&& get(std::in_place_index_t<I>) && noexcept {
-    return std::move(rest_).get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr const auto&& get(std::in_place_index_t<I>) const&& noexcept {
-    return std::move(rest_).get(std::in_place_index<I - 1>);
-  }
-};
-
-template <typename T, typename... Ts> union union_storage_impl<true, T, Ts...> {
-  T first_;
-  union_storage<Ts...> rest_;
-
-  template <typename... Args>
-  constexpr union_storage_impl(std::in_place_index_t<0>, Args&&... args)
-      : first_(static_cast<Args&&>(args)...) {}
-
-  template <std::size_t I, typename... Args>
-  constexpr union_storage_impl(std::in_place_index_t<I>, Args&&... args)
-      : rest_(std::in_place_index<I - 1>, static_cast<Args&&>(args)...) {}
-
-  ~union_storage_impl() = default;
-
-  template <typename... Args>
-  constexpr void emplace_impl(std::false_type, std::in_place_index_t<0>,
-                              Args&&... args) {
-    std::construct_at(&first_, static_cast<Args&&>(args)...);
-  }
-
-  template <std::size_t I, typename... Args>
-  constexpr void emplace_impl(std::false_type, std::in_place_index_t<I>,
-                              Args&&... args) {
-    rest_.emplace(std::in_place_index<I - 1>, static_cast<Args&&>(args)...);
-  }
-
-  template <std::size_t I, typename... Args>
-  constexpr void emplace_impl(std::true_type, std::in_place_index_t<I>,
-                              Args&&... args) {
-    *this = union_storage_impl(std::in_place_index<I>,
-                               static_cast<Args&&>(args)...);
-  }
-
-  template <std::size_t I, typename... Args>
-  constexpr void emplace(std::in_place_index_t<I>, Args&&... args) {
-    return emplace_impl(
-        std::conjunction<std::is_trivially_move_assignable<T>,
-                         std::is_trivially_move_assignable<Ts>...>{},
-        std::in_place_index<I>, static_cast<Args&&>(args)...);
-  }
-
-  constexpr void destroy(std::in_place_index_t<0>) noexcept {
-    if constexpr (!std::is_trivially_destructible_v<T>) {
-      std::destroy_at(&first_);
-    }
-  }
-
-  template <std::size_t I>
-  constexpr void destroy(std::in_place_index_t<I>) noexcept {
-    return rest_.destroy(std::in_place_index<I - 1>);
-  }
-
-  constexpr T& get(std::in_place_index_t<0>) & noexcept { return first_; }
-  constexpr const T& get(std::in_place_index_t<0>) const& noexcept {
-    return first_;
-  }
-  constexpr T&& get(std::in_place_index_t<0>) && noexcept {
-    return static_cast<T&&>(first_);
-  }
-  constexpr const T&& get(std::in_place_index_t<0>) const&& noexcept {
-    return static_cast<const T&&>(first_);
-  }
-
-  template <std::size_t I>
-  constexpr auto& get(std::in_place_index_t<I>) & noexcept {
-    return rest_.get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr const auto& get(std::in_place_index_t<I>) const& noexcept {
-    return rest_.get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr auto&& get(std::in_place_index_t<I>) && noexcept {
-    return std::move(rest_).get(std::in_place_index<I - 1>);
-  }
-
-  template <std::size_t I>
-  constexpr const auto&& get(std::in_place_index_t<I>) const&& noexcept {
-    return std::move(rest_).get(std::in_place_index<I - 1>);
-  }
-};
-
 // for use in a variant with a single element
 template <std::size_t I> class constant_index {
 public:
