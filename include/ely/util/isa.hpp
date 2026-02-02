4513a5320dc8f2dfb9731495e8b73a1c09b0b4e6
Frederik Engels <frederik.engels92@gmail.com>
2026-02-02 12:35:07 +0000

    allow isa with multiple alternatives

--- include/ely/util/isa.hpp
+++ include/ely/util/isa.hpp
@@ -6,7 +6,14 @@
 namespace ely {
 template <typename T, typename... Ts>
 constexpr bool isa(const ely::variant<Ts...>& v) {
-  return ely::visit([]<typename U>(U&) { return std::is_same_v<U, T>; }, const_cast<ely::variant<Ts...>&>(
-    v));
+  return ely::visit([]<typename U>(U&) { return std::is_same_v<U, T>; },
+                    const_cast<ely::variant<Ts...>&>(v));
+}
+
+template <typename... Us, typename... Ts>
+constexpr bool isa(const ely::variant<Ts...>& v) {
+  return ely::visit(
+      []<typename U>(U&) { return (std::is_same_v<U, Us> || ...); },
+      const_cast<ely::variant<Ts...>&>(v));
 }
 } // namespace ely
