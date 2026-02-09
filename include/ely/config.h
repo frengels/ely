5f2e4f493b711c11f36d2db44f474a48010c20d5
Frederik Engels <frederik.engels92@gmail.com>
2026-02-09 15:12:27 +0000

    add more attributes

--- include/ely/config.h
+++ include/ely/config.h
@@ -14,8 +14,32 @@
 #define ELY_MUSTTAIL
 #endif
 
+#if __has_attribute(preserve_none)
+#define ELY_PRESERVE_NONE __attribute__((preserve_none))
+#else
+#define ELY_PELY_PRESERVE_NONE
+#endif
+
+#if __has_attribute(cold)
+#define ELY_COLD __attribute__((cold))
+#else
+#define ELY_COLD
+#endif
+
+#if __has_attribute(hot)
+#define ELY_HOT __attribute__((hot))
+#else
+#define ELY_HOT
+#endif
+
 #if __has_attribute(always_inline)
 #define ELY_ALWAYS_INLINE inline __attribute__((always_inline))
 #else
 #define ELY_ALWAYS_INLINE inline
+#endif
+
+#if __has_attribute(noinline)
+#define ELY_NOINLINE __attribute__((noinline))
+#else
+#define ELY_NOINLINE
 #endif
