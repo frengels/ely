9ab30876171cf555813ffca04065defd5e36ee24
Frederik Engels <frederik.engels92@gmail.com>
2026-03-06 23:05:08 +0000

    this case is covered by the variadic one

--- include/ely/util/isa.hpp
+++ include/ely/util/isa.hpp
@@ -4,11 +4,6 @@
 #include "ely/util/visit.hpp"
 
 namespace ely {
-template <typename T, typename... Ts>
-constexpr bool isa(const ely::variant<Ts...>& v) {
-  return ely::visit([]<typename U>(U&) { return std::is_same_v<U, T>; },
-                    const_cast<ely::variant<Ts...>&>(v));
-}
 
 template <typename... Us, typename... Ts>
 constexpr bool isa(const ely::variant<Ts...>& v) {
