3ede80066c83fa69ea88e65a94ec39d0d0739f23
Frederik Engels <frederik.engels92@gmail.com>
2026-03-07 00:11:20 +0000

    cannot default initialize source_location

--- test/util.hpp
+++ test/util.hpp
@@ -4,8 +4,9 @@
 #include <source_location>
 
 template <typename T, typename U>
-constexpr void check_eq(const T& lhs, const U& rhs,
-                        std::source_location loc = {}) {
+constexpr void
+check_eq(const T& lhs, const U& rhs,
+         std::source_location loc = std::source_location::current()) {
   if (!(lhs == rhs)) {
     fmt::println("error {}:{}:{}:{}:", loc.file_name(), loc.line(),
                  loc.column(), loc.function_name());
