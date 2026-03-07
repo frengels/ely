4d4c185633ce5abf3e11e88bbdd1bbcbacff61c1
Frederik Engels <frederik.engels92@gmail.com>
2026-03-07 00:09:30 +0000

    test interner at runtime and compile-time

--- include/ely/interner.hpp
+++ include/ely/interner.hpp
@@ -1,21 +1,21 @@
 #pragma once
 
 #include <cstdint>
-#include <forward_list>
-#include <limits>
-#include <llvm/ADT/Hashing.h>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <vector>
 
-#include "ely/arena/block.hpp"
+#include "ely/arena/growing.hpp"
+#include "ely/hash/fnv.hpp"
 #include "ely/symbol.hpp"
+#include "ely/util/cx_or_rt.hpp"
+#include "ely/util/optional.hpp"
 
 namespace ely {
 // a simple interner implementation, currently doesn't do anything optimal
-template <typename CharT, typename Traits = std::char_traits<CharT>,
-          typename Arena = ely::arena::fixed_block<CharT, 256 * 1024>>
+template <typename CharT, typename Traits = std::char_traits<CharT>>
 class basic_simple_interner {
 public:
   using char_type = CharT;
@@ -23,38 +23,61 @@ public:
   using string_view_type = std::basic_string_view<CharT, Traits>;
 
 private:
-  std::unordered_map<string_view_type, symbol_type> map_;
+  ely::cx_or_rt<std::vector<std::pair<string_view_type, symbol_type>>,
+                std::unordered_map<string_view_type, symbol_type>>
+      map_storage_;
+  // std::unordered_map<string_view_type, symbol_type> map_;
   std::vector<string_view_type> ref_;
-  Arena* alloc_;
+  ely::arena::growing arena_{4096};
 
 public:
-  constexpr basic_simple_interner(Arena& alloc)
-      : alloc_(std::addressof(alloc)) {}
+  basic_simple_interner() = default;
 
   constexpr symbol_type intern(string_view_type strv) {
-    auto it = map_.find(strv);
-    if (it != map_.end())
-      return it->second;
+    return map_storage_
+        .visit(
+            [&](const auto& vec) -> ely::optional<symbol_type> {
+              for (auto target : vec) {
+                if (strv == target.first) {
+                  return target.second;
+                }
+              }
 
-    char_type* p = alloc_->allocate(strv.size());
-    std::uninitialized_copy(strv.begin(), strv.end(), p);
-    string_view_type internal = string_view_type{p, strv.size()};
-    // std::string& ref = buffer_.emplace_front(strv);
+              return ely::nullopt;
+            },
+            [&](const auto& map) -> ely::optional<symbol_type> {
+              auto it = map.find(strv);
+              if (it != map.end()) {
+                return it->second;
+              }
 
-    // add references to this location in both the map and ref
-    auto id = symbol_type{static_cast<std::uint32_t>(ref_.size() + 1)};
-    ref_.emplace_back(internal);
-    map_.emplace(std::piecewise_construct, std::forward_as_tuple(internal),
-                 std::forward_as_tuple(id));
-    return id;
+              return ely::nullopt;
+            })
+        .value_or_else([&]() {
+          char_type* p = arena_.allocate<char>(strv.size());
+          std::copy(strv.begin(), strv.end(), p);
+          string_view_type internal = string_view_type{p, strv.size()};
+
+          auto id = symbol_type{static_cast<std::uint32_t>(ref_.size())};
+          ref_.emplace_back(internal);
+
+          map_storage_.visit(
+              [&](std::vector<std::pair<string_view_type, symbol_type>>& vec) {
+                vec.emplace_back(internal, id);
+              },
+              [&](auto& map) {
+                map.emplace(std::piecewise_construct,
+                            std::forward_as_tuple(internal),
+                            std::forward_as_tuple(id));
+              });
+          return id;
+        });
   }
 
   constexpr string_view_type lookup(symbol_type sym) const {
-    return ref_[static_cast<std::size_t>(sym.id) - 1];
+    return ref_[static_cast<std::size_t>(sym.id)];
   }
 };
 
-template <typename Alloc>
-using simple_interner =
-    basic_simple_interner<char, std::char_traits<char>, Alloc>;
+using simple_interner = basic_simple_interner<char, std::char_traits<char>>;
 } // namespace ely
--- test/interner.cpp
+++ test/interner.cpp
@@ -6,51 +6,10 @@
 
 #include "support.hpp"
 
-#include "ely/arena/block.hpp"
-
 #include <string_view>
 
-class data {
-private:
-  struct data_storage {
-    int x, y;
-    std::string name;
-
-    using key_type = std::tuple<int, int>;
-
-  public:
-    data_storage(int x, int y, std::string name)
-        : x(x), y(y), name(std::move(name)) {}
-
-    static key_type get_key(int x, int y, const std::string&) {
-      return key_type{x, y};
-    }
-  };
-
-public:
-  using storage_type = data_storage;
-
-private:
-  storage_type* impl_;
-
-public:
-  data() = default;
-  // construct from store pointer
-  constexpr data(storage_type* store) : impl_(store) {}
-
-  // simple pointer equality
-  friend constexpr bool operator==(const data& lhs, const data& rhs) {
-    return lhs.impl_ == rhs.impl_;
-  }
-
-  constexpr int x() const { return impl_->x; }
-  constexpr int y() const { return impl_->y; }
-  constexpr std::string_view name() const { return impl_->name; }
-};
-
-bool simple_interner() {
-  auto arena = ely::arena::fixed_block<char, 128 * 1024>{};
-  auto interner = ely::simple_interner(arena);
+constexpr bool simple_interner() {
+  auto interner = ely::simple_interner();
 
   // 2 equivalent interned symbol must compare equal
   auto hello_sym = interner.intern("hello");
@@ -70,26 +29,26 @@ bool simple_interner() {
   return true;
 }
 
-template <typename T> bool test_interner2() {
-  auto intern = ely::interner2<T>{};
-  T d0 = intern.intern(4, 5, "Hello");
-  T d1 = intern.intern(4, 5, "Bye");
+// template <typename T> bool test_interner2() {
+//   auto intern = ely::interner2<T>{};
+//   T d0 = intern.intern(4, 5, "Hello");
+//   T d1 = intern.intern(4, 5, "Bye");
 
-  assert(d0 == d1);
-  assert(d0.name() == "Hello");
-  assert(d1.name() == "Hello");
+//   assert(d0 == d1);
+//   assert(d0.name() == "Hello");
+//   assert(d1.name() == "Hello");
 
-  T d2 = intern.intern(5, 4, "Yes");
+//   T d2 = intern.intern(5, 4, "Yes");
 
-  assert(d2 != d0);
+//   assert(d2 != d0);
 
-  return true;
-}
+//   return true;
+// }
 
 void interner() {
   assert(simple_interner());
+  static_assert(simple_interner());
   fmt::println("ely/simple_interner - SUCCESS");
-  assert(test_interner2<data>());
 }
 
 #ifndef NO_MAIN
